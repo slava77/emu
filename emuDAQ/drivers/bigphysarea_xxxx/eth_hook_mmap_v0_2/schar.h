c289997ebf28d12b5552c9f81ab40622ddb0b41d
geurts <geurts@4525493e-7705-40b1-a816-d608a930855b>
2005-09-22 20:21:43 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@2410 4525493e-7705-40b1-a816-d608a930855b

---
+++ emuDAQ/drivers/bigphysarea_xxxx/eth_hook_mmap_v0_2/schar.h
@@ -0,0 +1,23 @@
+#define SCHAR_INC		1024
+#define SCHAR_TIMER_DELAY	5*HZ
+#define SCHAR_POOL_MIN		10*1024
+
+#define SCHAR_MAX_SYSCTL	1024
+#define DEV_SCHAR		10
+#define DEV_SCHAR_ENTRY		1
+
+/* ioctl's for schar. */
+#define SCHAR_IOCTL_BASE	0xbb
+#define SCHAR_RESET     	_IO(SCHAR_IOCTL_BASE, 0)
+#define SCHAR_PAGES		_IOR(SCHAR_IOCTL_BASE, 1, 0)
+#define SCHAR_RING		_IOR(SCHAR_IOCTL_BASE, 2, 0)
+#define SCHAR_PMISSING		_IOR(SCHAR_IOCTL_BASE, 3, 0)
+#define SCHAR_EEVENT		_IOR(SCHAR_IOCTL_BASE, 4, 0)
+
+#define DEBUG
+
+#ifdef DEBUG
+#define MSG(string, args...) if (schar_debug) printk(KERN_DEBUG "schar: " string, ##args)
+#else
+#define MSG(string, args...)
+#endif
