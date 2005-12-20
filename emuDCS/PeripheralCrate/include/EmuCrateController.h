22c719fde6bf09d920a083f68ce2a97252d12640
mey <mey>
2005-12-20 14:38:07 +0000

    Update

---
+++ emuDCS/PeripheralCrate/include/EmuCrateController.h
@@ -0,0 +1,61 @@
+//-----------------------------------------------------------------------
+// $Id: EmuCrateController.h,v 1.1 2005/12/20 14:38:07 mey Exp $
+// $Log: EmuCrateController.h,v $
+// Revision 1.1  2005/12/20 14:38:07  mey
+// Update
+//
+// Revision 2.2  2005/09/13 14:46:39  mey
+// Get DMB crate id; and DCS
+//
+// Revision 2.1  2005/08/11 08:13:59  mey
+// Update
+//
+// Revision 2.0  2005/04/12 08:07:03  geurts
+// *** empty log message ***
+//
+// Revision 1.7  2004/06/18 23:52:33  tfcvs
+// Introduced code for DCS/PCcontrol crate sharing. (FG)
+//  -code additions still commented out, awaiting validation-
+//
+// Revision 1.6  2004/06/01 09:52:07  tfcvs
+//  enabled CCB in enable(), added CVS-stuff, cleaned up includes (FG)
+//
+//-----------------------------------------------------------------------
+#ifndef EmuCrateController_h
+#define EmuCrateController_h
+#include "CrateSelector.h"
+#include <string>
+
+class CCB;
+class Crate;
+
+class EmuCrateController {
+public:
+  EmuCrateController();
+  ~EmuCrateController() {}
+
+  void configure();
+
+  void enable();
+
+  void disable();
+
+  CrateSelector & selector() {return theSelector;}
+
+  /// sends the command to the appropriate boards selected by the CrateSelector
+  /// boardType comes from VMEModule.h
+  void executeCommand(std::string boardType, std::string command);
+
+protected:
+  void configure(Crate * crate);
+  void configure(CCB * ccb);
+
+  void enable(Crate * crate);
+
+  void disable(Crate * crate);
+
+private:
+  CrateSelector theSelector;
+};
+
+#endif
