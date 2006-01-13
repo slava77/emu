a4cad4132a153511602e0599c639630f596334b3
mey <mey>
2006-01-13 21:05:03 +0000

    RunCOntrol

---
+++ emuDCS/PeripheralCrate/src/common/EmuRunControlHyperDAQ.cc
@@ -0,0 +1,19 @@
+// $Id: EmuRunControlHyperDAQ.cc,v 1.1 2006/01/13 21:04:33 mey Exp $
+
+/*************************************************************************
+ * XDAQ Components for Distributed Data Acquisition                      *
+ * Copyright (C) 2000-2004, CERN.			                 *
+ * All rights reserved.                                                  *
+ * Authors: J. Gutleber and L. Orsini					 *
+ *                                                                       *
+ * For the licensing terms see LICENSE.		                         *
+ * For the list of contributors see CREDITS.   			         *
+ *************************************************************************/
+
+#include "EmuRunControlHyperDAQ.h"
+
+//
+// provides factory method for instantion of SimpleSOAPSender application
+//
+XDAQ_INSTANTIATOR_IMPL(EmuRunControlHyperDAQ)
+  //
