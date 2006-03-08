79e0d91bcb6de1de5d9d97b81b3dadb46abf08b8
ichiro <ichiro>
2006-03-08 15:08:41 +0000

    added EmuFED, which doesn't have Enabled state

---
+++ cscSV/test/EmuFED/src/common/EmuFEDV.cc
@@ -0,0 +1,32 @@
+//
+// EmuFEDV.cc
+
+#include "EmuFEDV.h"
+
+#include "toolboxV.h"
+#include "xoap/version.h"
+#include "xdaqV.h"
+
+GETPACKAGEINFO(EmuFED)
+
+void EmuFED::checkPackageDependencies()
+		throw (toolbox::PackageInfo::VersionException)
+{
+	CHECKDEPENDENCY(toolbox)
+	CHECKDEPENDENCY(xoap)
+	CHECKDEPENDENCY(xdaq)
+}
+
+set<string, less<string> > xdaq::getPackageDependencies()
+{
+	set<string, less<string> > dependencies;
+
+	ADDDEPENDENCY(dependencies, toolbox);
+	ADDDEPENDENCY(dependencies, xoap);
+	ADDDEPENDENCY(dependencies, xdaq);
+
+	return dependencies;
+}
+
+// End of file
+// vim: set ai sw=4 ts=4:
