52041929fb686f9c6590667220b8bae5e54d8066
mey <mey@4525493e-7705-40b1-a816-d608a930855b>
2006-06-20 12:24:58 +0000

    New file
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@3090 4525493e-7705-40b1-a816-d608a930855b

---
+++ emuDCS/PeripheralCore/include/EmuModule.h
@@ -0,0 +1,25 @@
+#ifndef EmuModule_h
+#define EmuModule_h
+
+
+#include <iostream>
+
+class EmuModule
+{
+public:
+  //
+  EmuModule();
+  virtual ~EmuModule() {};
+  
+  inline void RedirectOutput(std::ostream * Output) { MyOutput_ = Output ; }
+  
+  void SendOutput(std::string);
+  
+  std::ostream * MyOutput_ ;
+
+private:
+  //
+  //
+};
+
+#endif
