a6166ed699156ed327cfed66bbf890a8579fbe21
mey <mey>
2006-06-23 13:47:23 +0000

    Update

--- emuDCS/PeripheralCore/include/EmuModule.h
+++ emuDCS/PeripheralCore/include/EmuModule.h
@@ -11,11 +11,11 @@ public:
   EmuModule();
   virtual ~EmuModule() {};
   
-  inline void RedirectOutput(std::ostream * Output) { MyOutput_ = Output ; }
+  //inline void RedirectOutput(std::ostream * Output) { MyOutput_ = Output ; }
   
   void SendOutput(std::string,std::string = "INFO");
   
-  std::ostream * MyOutput_ ;
+  //std::ostream * MyOutput_ ;
 
 private:
   //
