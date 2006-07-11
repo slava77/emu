d78836409a8cead835f7d9479883ce39badfcef0
mey <mey@4525493e-7705-40b1-a816-d608a930855b>
2006-07-11 15:00:39 +0000

    Update
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@3154 4525493e-7705-40b1-a816-d608a930855b

--- emuDCS/PeripheralCrate/include/TMBParser.h
+++ emuDCS/PeripheralCrate/include/TMBParser.h
@@ -1,6 +1,9 @@
 //-----------------------------------------------------------------------
-// $Id: TMBParser.h,v 2.1 2006/03/30 13:55:38 mey Exp $
+// $Id: TMBParser.h,v 2.2 2006/07/11 15:00:38 mey Exp $
 // $Log: TMBParser.h,v $
+// Revision 2.2  2006/07/11 15:00:38  mey
+// Update
+//
 // Revision 2.1  2006/03/30 13:55:38  mey
 // Update
 //
@@ -15,7 +18,7 @@
 #include <xercesc/dom/DOM.hpp>
 
 class TMB;
-
+class Chamber;
 class TMBParser
 {
 public:
@@ -26,9 +29,12 @@ public:
   /// returns the last tmb parsed
   TMB * tmb() const {return tmb_;}
 
+  inline void SetCSC(Chamber * csc){csc_ = csc;}
+
 private:
   TMB * tmb_;
   EmuParser parser_;
+  Chamber * csc_;
 };
 
 #endif
--- emuDCS/PeripheralCrate/src/common/CSCParser.cc
+++ emuDCS/PeripheralCrate/src/common/CSCParser.cc
@@ -1,6 +1,9 @@
 //-----------------------------------------------------------------------
-// $Id: CSCParser.cc,v 1.3 2006/05/31 12:37:26 mey Exp $
+// $Id: CSCParser.cc,v 1.4 2006/07/11 15:00:39 mey Exp $
 // $Log: CSCParser.cc,v $
+// Revision 1.4  2006/07/11 15:00:39  mey
+// Update
+//
 // Revision 1.3  2006/05/31 12:37:26  mey
 // Update
 //
@@ -39,11 +42,13 @@ CSCParser::CSCParser(xercesc::DOMNode * pNode, int crateNumber)
       //
       if (strcmp("DAQMB",xercesc::XMLString::transcode(pNode1->getNodeName()))==0) {  
 	daqmbParser_ = DAQMBParser(pNode1, crateNumber);
+	daqmbParser_.SetCSC(csc_);
 	csc_->SetDMB(daqmbParser_.daqmb());
       }
       //
       if (strcmp("TMB",xercesc::XMLString::transcode(pNode1->getNodeName()))==0) {  
 	tmbParser_ = TMBParser(pNode1, crateNumber);
+	tmbParser_.SetCSC(csc_);
 	csc_->SetTMB(tmbParser_.tmb());
       }       
     }
