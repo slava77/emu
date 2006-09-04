9929049d599243306ae411db83b41a3d3aa4eb01
mey <mey@4525493e-7705-40b1-a816-d608a930855b>
2006-09-04 11:56:24 +0000

    UPdate
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@3298 4525493e-7705-40b1-a816-d608a930855b

--- emuDCS/PeripheralCrate/bin/startLoggingServer.sh
+++ emuDCS/PeripheralCrate/bin/startLoggingServer.sh
@@ -1,2 +1,2 @@
 cd $XDAQ_ROOT/daq/extern/log4cplus/linuxx86/src/
-./loggingserver 60000 $XDAQ_ROOT/daq/extern/log4cplus/etc/loggingserver.config
+./loggingserver 2924 $XDAQ_ROOT/daq/extern/log4cplus/etc/loggingserver.config
--- emuDCS/PeripheralCrate/bin/startPeripheralCrate.sh
+++ emuDCS/PeripheralCrate/bin/startPeripheralCrate.sh
@@ -1,4 +1,18 @@
-#!/bin/tcsh
+#!/bin/sh
+# Source function library.
+if [ -f /etc/init.d/functions ] ; then
+        . /etc/init.d/functions
+else
+    echo -n 'ERROR xdaqjc: Could not find /etc/init.d/functions, stopping'
+    exit -1
+fi
+#
+killproc xdaq.exe
+#
 cd $XDAQ_ROOT/emu/emuDCS/PeripheralCrate/
-if( -test xdaq.log ) rm xdaq.log
-$XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 40010 -c xml/EmuCluster.xml -l INFO
+#
+xterm -e $XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 2924 -c xml/EmuCluster.xml -l INFO &
+xterm -e $XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 2925 -c xml/EmuCluster.xml -l INFO &
+xterm -e $XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 2926 -c xml/EmuCluster.xml -l INFO &
+xterm -e $XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 2927 -c xml/EmuCluster.xml -l INFO &
+xterm -geometry 200x20+20+0 -e $XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 2923 -c xml/EmuCluster.xml -l INFO &
