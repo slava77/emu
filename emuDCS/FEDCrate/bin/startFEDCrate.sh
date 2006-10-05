6544c3d8aa1ca4897c46f5ecab100383205439e7
mey <mey@4525493e-7705-40b1-a816-d608a930855b>
2006-10-05 09:21:41 +0000

    Update
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@3380 4525493e-7705-40b1-a816-d608a930855b

--- emuDCS/FEDCrate/bin/startFEDCrate.sh
+++ emuDCS/FEDCrate/bin/startFEDCrate.sh
@@ -1,4 +1,13 @@
-#!/bin/tcsh
+#!/bin/sh
+# Source function library.
+if [ -f /etc/init.d/functions ] ; then
+        . /etc/init.d/functions
+else
+    echo -n 'ERROR xdaqjc: Could not find /etc/init.d/functions, stopping'
+    exit -1
+fi
+#
+killproc xdaq.exe
+#
 cd $XDAQ_ROOT/emu/emuDCS/FEDCrate/
-if( -test xdaq.log ) rm xdaq.log
 $XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 40100 -c xml/EmuFCluster.xml -l INFO
