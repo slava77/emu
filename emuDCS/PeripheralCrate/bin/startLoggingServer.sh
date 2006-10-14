6a062a0ca1d96287eccefe8581d8cec03475d7dc
mey <mey@4525493e-7705-40b1-a816-d608a930855b>
2006-10-14 10:08:00 +0000

    Update
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@3408 4525493e-7705-40b1-a816-d608a930855b

--- emuDCS/PeripheralCrate/bin/startLoggingServer.sh
+++ emuDCS/PeripheralCrate/bin/startLoggingServer.sh
@@ -1,2 +1,13 @@
+#!/bin/sh
+# Source function library.
+if [ -f /etc/init.d/functions ] ; then
+        . /etc/init.d/functions
+else
+    echo -n 'ERROR xdaqjc: Could not find /etc/init.d/functions, stopping'
+    exit -1
+fi
+#
+killproc lt-loggingserver
+#
 cd $XDAQ_ROOT/daq/extern/log4cplus/linuxx86/src/
 ./loggingserver 2924 $XDAQ_ROOT/daq/extern/log4cplus/etc/loggingserver.config
