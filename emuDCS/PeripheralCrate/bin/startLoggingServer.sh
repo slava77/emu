a99edcc2142f4287f389d27144d73da613d7a44a
mey <mey>
2006-10-24 08:54:53 +0000

    UPdate

--- emuDCS/PeripheralCrate/bin/startLoggingServer.sh
+++ emuDCS/PeripheralCrate/bin/startLoggingServer.sh
@@ -7,7 +7,8 @@ else
     exit -1
 fi
 #
-killproc lt-loggingserver
+killall -q /home/cscpc/DAQkit/v3.9/TriDAS/daq/extern/log4cplus/linuxx86/src/.libs/lt-loggingserver
+#killproc lt-loggingserver
 #
 cd $XDAQ_ROOT/daq/extern/log4cplus/linuxx86/src/
-./loggingserver 2924 $XDAQ_ROOT/daq/extern/log4cplus/etc/loggingserver.config
+nohup ./loggingserver 2924 $XDAQ_ROOT/daq/extern/log4cplus/etc/loggingserver.config >& /tmp/logging.log &
