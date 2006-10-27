a914a668fbeba55438b01f5a1fd674d5f7faa0ef
banicz <banicz>
2006-10-27 21:42:53 +0000

    Increase MAXEVENT size.

--- emuDAQ/drivers/bigphysarea_xxxx/eth_hook_mmap_v0_2/eth_hook_2.h
+++ emuDAQ/drivers/bigphysarea_xxxx/eth_hook_mmap_v0_2/eth_hook_2.h
@@ -4,7 +4,8 @@
 #define RING_ENTRY_LENGTH 8
 #define MAXPACKET_2 9100
 //#define MAXEVENT_2 30100
-#define MAXEVENT_2 75601
+// #define MAXEVENT_2 75601
+#define MAXEVENT_2 128000// try an even bigger one to accomodate monsters
 #define TAILMEM 100
 #define TAILPOS  80
 #define SKB_EXTRA 14
