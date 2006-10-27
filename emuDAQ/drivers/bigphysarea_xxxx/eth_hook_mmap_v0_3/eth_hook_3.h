50c4dba69d1778ca43ea6d1614c3a9203c28aa20
banicz <banicz@4525493e-7705-40b1-a816-d608a930855b>
2006-10-27 21:42:53 +0000

    Increase MAXEVENT size.
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@3483 4525493e-7705-40b1-a816-d608a930855b

--- emuDAQ/drivers/bigphysarea_xxxx/eth_hook_mmap_v0_3/eth_hook_3.h
+++ emuDAQ/drivers/bigphysarea_xxxx/eth_hook_mmap_v0_3/eth_hook_3.h
@@ -4,7 +4,8 @@
 #define RING_ENTRY_LENGTH 8
 #define MAXPACKET_3 9100
 //#define MAXEVENT_2 30100
-#define MAXEVENT_3 75601
+// #define MAXEVENT_3 75601
+#define MAXEVENT_3 128000// try an even bigger one to accomodate monsters
 #define TAILMEM 100
 #define TAILPOS  80
 #define SKB_EXTRA 14
