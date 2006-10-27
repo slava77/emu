d32b417a4a9a1fb16cd688ad734a70cb3ae9118c
ichiro <ichiro@4525493e-7705-40b1-a816-d608a930855b>
2006-10-27 14:37:59 +0000

    test application of TTCciControl
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@3477 4525493e-7705-40b1-a816-d608a930855b

---
+++ cscSV/test/TTC/include/TTCciControlV.h
@@ -0,0 +1,18 @@
+#ifndef __TTCCI_CONTROL_V_H__
+#define __TTCCI_CONTROL_V_H__
+
+#include "PackageInfo.h"
+
+namespace TTCciControl {
+	const string package     = "TTCciControl";
+	const string versions    = "1.0";
+	const string description = "dummy XDAQ application to test CSC Supervisor";
+	const string link        = "http://localhost:40000/";
+
+	toolbox::PackageInfo getPackageInfo();
+	void checkPackageDependencies() throw (toolbox::PackageInfo::VersionException);
+	set<string, less<string> > getPackageDependencies();
+};
+
+#endif  // ifndef __TTCCI_CONTROL_V_H__
+// vim: set ai sw=4 ts=4:
