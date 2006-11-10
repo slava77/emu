240a7a15a8f826e1c3d68ff3ce76428656e4c6f1
mey <mey>
2006-11-10 14:01:48 +0000

    UPdate

--- emuDCS/PeripheralCrate/bin/startPeripheralCrate.sh
+++ emuDCS/PeripheralCrate/bin/startPeripheralCrate.sh
@@ -7,10 +7,23 @@ else
     exit -1
 fi
 #
-killproc xdaq.exe
+#killproc xdaq.exe
+killall -q /home/cscpc/DAQkit/v3.9/TriDAS/daq/xdaq/bin/linux/x86/xdaq.exe
 #
 cd $XDAQ_ROOT/emu/emuDCS/PeripheralCrate/
 #
-$XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 1971 -c xml/EmuCluster.xml -l INFO &
-
-~/startLoggingServer.sh
+#xterm -e $XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 40011 -c xml/EmuCluster.xml -l INFO &
+#xterm -e $XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 40012 -c xml/EmuCluster.xml -l INFO &
+#xterm -e $XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 40013 -c xml/EmuCluster.xml -l INFO &
+#xterm -e $XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 40014 -c xml/EmuCluster.xml -l INFO &
+#xterm -geometry 200x20+20+0 -e $XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 40010 -c xml/EmuCluster.xml -l INFO &
+#
+nohup $XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 40011 -c xml/EmuCluster.xml -l INFO >& /tmp/xdaq40011.log &
+nohup $XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 40012 -c xml/EmuCluster.xml -l INFO >& /tmp/xdaq40012.log &
+nohup $XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 40013 -c xml/EmuCluster.xml -l INFO >& /tmp/xdaq40013.log &
+nohup $XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 40014 -c xml/EmuCluster.xml -l INFO >& /tmp/xdaq40014.log &
+nohup $XDAQ_ROOT/daq/xdaq/bin/linux/x86/xdaq.sh -p 40010 -c xml/EmuCluster.xml -l INFO >& /tmp/xdaq40010.log &
+#
+#~/startLoggingServer.sh
+#
+exit
