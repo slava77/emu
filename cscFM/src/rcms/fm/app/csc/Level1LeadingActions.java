b31e92d92dbf5703f8ac39b0ee27283bffc8adb4
ichiro <ichiro@4525493e-7705-40b1-a816-d608a930855b>
2007-02-14 16:22:11 +0000

    new baseline function manager only TTS test related state transitions are implemented now.
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@3591 4525493e-7705-40b1-a816-d608a930855b

---
+++ cscFM/src/rcms/fm/app/csc/Level1LeadingActions.java
@@ -0,0 +1,111 @@
+package rcms.fm.app.csc;
+
+import rcms.fm.fw.user.*;
+import rcms.fm.fw.EventHandlerException;
+import rcms.fm.fw.parameter.*;
+import rcms.fm.fw.parameter.type.*;
+import rcms.fm.fw.StateEnteredEvent;
+import rcms.fm.resource.QualifiedGroup;
+import rcms.stateFormat.StateNotification;
+
+import rcms.util.logger.RCMSLogger;
+
+public class Level1LeadingActions extends UserEventHandler {
+
+    private RCMSLogger logger = new RCMSLogger(Level1FunctionManager.class);
+
+	Level1FunctionManager fm;
+
+	/*
+	 *
+	 */
+	public Level1LeadingActions() throws EventHandlerException {
+		subscribeForEvents(StateEnteredEvent.class);
+
+		addAction(Level1States.INITIALIZING,  "initialize");
+		addAction(Level1States.TTS_PREPARING, "ttsPrepare");
+		addAction(Level1States.TTS_TESTING,   "ttsTest");
+		addAction(Level1States.HALTING,       "halt");
+	}
+
+	/*
+	 *
+	 */
+	public void init() throws EventHandlerException {
+		fm = (Level1FunctionManager)getUserFunctionManager();
+	}
+
+	/*
+	 *
+	 */
+	public void initialize(Object o) throws UserActionException {
+		logger.debug("Level1LeadingActions.initialize");
+
+		fm.getParameterSet().put(new FunctionManagerParameter<StringT>(
+				Level1Parameters.ACTION_MSG, new StringT("Initializing")));
+
+		QualifiedGroup group = fm.getQualifiedGroup();
+
+		// Initialize the qualified group, whatever it means.
+		try {
+			group.init();
+		} catch (Exception e) {
+			logger.error(getClass().toString() +
+					"Failed to initialize resources.", e);
+
+			fm.fireEvent(Level1Inputs.ERROR);
+		}
+
+		fm.fireEvent(createStateNotification());
+	}
+
+	/*
+	 *
+	 */
+	public void ttsPrepare(Object o) throws UserActionException {
+		logger.debug("Level1LeadingActions.ttsPrepare");
+
+		fm.getParameterSet().put(new FunctionManagerParameter<StringT>(
+				Level1Parameters.ACTION_MSG, new StringT("TTS preparing")));
+
+		fm.fireEvent(createStateNotification());
+	}
+
+	/*
+	 *
+	 */
+	public void ttsTest(Object o) throws UserActionException {
+		logger.debug("Level1LeadingActions.ttsTest");
+
+		fm.getParameterSet().put(new FunctionManagerParameter<StringT>(
+				Level1Parameters.ACTION_MSG, new StringT("TTS testing")));
+
+		fm.fireEvent(createStateNotification());
+	}
+
+	/*
+	 *
+	 */
+	public void halt(Object o) throws UserActionException {
+		logger.debug("Level1LeadingActions.halt");
+
+		fm.getParameterSet().put(new FunctionManagerParameter<StringT>(
+				Level1Parameters.ACTION_MSG, new StringT("Halting")));
+
+		fm.fireEvent(createStateNotification());
+	}
+
+	/*
+	 *
+	 */
+	public StateNotification createStateNotification() throws UserActionException {
+		StateNotification notification = new StateNotification();
+
+		notification.setIdentifier("cscFM");
+
+		return notification;
+	}
+}
+
+// End of file
+// vim: set sw=4 ts=4:
---
+++ cscFM/src/rcms/fm/app/csc/Level1StateMachineDefinition.java
@@ -0,0 +1,117 @@
+package rcms.fm.app.csc;
+
+import rcms.fm.fw.user.UserStateMachineDefinition;
+
+import rcms.statemachine.definition.*;
+
+import rcms.fm.fw.parameter.*;
+import rcms.fm.fw.parameter.type.*;
+
+import java.lang.reflect.Field;
+
+public class Level1StateMachineDefinition extends UserStateMachineDefinition {
+
+	public Level1StateMachineDefinition()
+			throws StateMachineDefinitionException {
+		//
+		// Declare states
+		addStates(Level1States.class);
+		setInitialState(Level1States.INITIAL);
+
+		//
+		// Declare inputs
+		addInputs(Level1Inputs.class);
+
+		//
+		// Declare parameters associated with inputs
+		// (initialize, configure, start and testTTS)
+
+		ParameterSet<CommandParameter> initializeParameters =
+				new ParameterSet<CommandParameter>();
+
+		try {
+			initializeParameters.add(new CommandParameter<StringT>(
+					Level1Parameters.SID, new StringT("")));
+			initializeParameters.add(new CommandParameter<StringT>(
+					Level1Parameters.GLOBAL_CONF_KEY, new StringT("")));
+		} catch (ParameterException ignored) {}
+
+		Level1Inputs.INITIALIZE.setParameters(initializeParameters);
+
+		ParameterSet<CommandParameter> testTTSParameters =
+				new ParameterSet<CommandParameter>();
+
+		try {
+			testTTSParameters.add(new CommandParameter<IntegerT>(
+					Level1Parameters.TTS_TEST_FED_ID, new IntegerT(-1)));
+			testTTSParameters.add(new CommandParameter<StringT>(
+					Level1Parameters.TTS_TEST_MODE, new StringT("")));
+			testTTSParameters.add(new CommandParameter<StringT>(
+					Level1Parameters.TTS_TEST_PATTERN, new StringT("")));
+			testTTSParameters.add(new CommandParameter<IntegerT>(
+					Level1Parameters.TTS_TEST_SEQUENCE_REPEAT, new IntegerT(-1)));
+		} catch (ParameterException ignored) {}
+
+		Level1Inputs.TTS_TEST.setParameters(testTTSParameters);
+
+		// Declare transitions
+		addTransition(Level1Inputs.INITIALIZE,
+				Level1States.INITIAL, Level1States.INITIALIZING);
+		addTransition(Level1Inputs.INITIALIZE_DONE,
+				Level1States.INITIALIZING, Level1States.HALTED);
+		addTransition(Level1Inputs.TTS_PREPARE,
+				Level1States.HALTED, Level1States.TTS_PREPARING);
+		addTransition(Level1Inputs.TTS_PREPARE_DONE,
+				Level1States.TTS_PREPARING, Level1States.TTS_READY);
+		addTransition(Level1Inputs.TTS_TEST,
+				Level1States.TTS_READY, Level1States.TTS_TESTING);
+		addTransition(Level1Inputs.TTS_TEST_DONE,
+				Level1States.TTS_TESTING, Level1States.TTS_READY);
+		addTransition(Level1Inputs.HALT,
+				Level1States.TTS_READY, Level1States.HALTING);
+		addTransition(Level1Inputs.HALT_DONE,
+				Level1States.HALTING, Level1States.HALTED);
+
+		addTransition(Level1Inputs.ERROR, State.ANYSTATE, Level1States.ERROR);
+	}
+
+	private void addStates(Class<Level1States> klass)
+			throws StateMachineDefinitionException {
+		Field fields[] = klass.getFields();
+
+		try {
+			for (int i = 0; i < fields.length; ++i) {
+				System.out.println(
+						"==== addStates: " + fields[i].getType().getName());
+				if (fields[i].getType().getName().indexOf("State") >= 0) {
+					addState((State)fields[i].get(null));
+				}
+			}
+		} catch (Exception e) {
+			throw new StateMachineDefinitionException("addStates", e);
+		}
+	}
+
+	private void addInputs(Class<Level1Inputs> klass)
+			throws StateMachineDefinitionException {
+		Field fields[] = klass.getFields();
+
+		try {
+			for (int i = 0; i < fields.length; ++i) {
+				System.out.println(
+						"==== addInputs: " + fields[i].getType().getName());
+				if (fields[i].getType().getName().indexOf("Input") >= 0) {
+					if (fields[i].getName().indexOf("_DONE") > 0) {
+						((Input)fields[i].get(null)).setVisualizable(false);
+					}
+					addInput((Input)fields[i].get(null));
+				}
+			}
+		} catch (Exception e) {
+			throw new StateMachineDefinitionException("addInputs", e);
+		}
+	}
+}
+
+// End of file
+// vim: set sw=4 ts=4:
