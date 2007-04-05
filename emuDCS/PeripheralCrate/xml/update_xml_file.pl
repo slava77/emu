e671c2ef165ec2d6de4239f5343f250fe511f20d
rakness <rakness@4525493e-7705-40b1-a816-d608a930855b>
2007-04-05 15:03:07 +0000

    fix bug for rpc0_rat_delay in ALCT section
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@3721 4525493e-7705-40b1-a816-d608a930855b

--- emuDCS/PeripheralCrate/xml/update_xml_file.pl
+++ emuDCS/PeripheralCrate/xml/update_xml_file.pl
@@ -165,6 +165,7 @@ foreach $parameter (@parameters) {
 	}
 #
 	if (($parameter =~ m/delay/) && 
+	    !($parameter =~ m/rpc0_rat_delay/) && 
 	    !($parameter =~ m/alct_drift_delay/) && 
 	    !($parameter =~ m/alct_l1a_delay/) ) { 
 	    $parameter =~ s/delay/afeb_fine_delay/;
