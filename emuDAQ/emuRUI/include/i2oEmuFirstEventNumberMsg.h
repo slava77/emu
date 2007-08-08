70f025e8c848a3cfb79833d602a5059299928ea8
banicz <banicz@4525493e-7705-40b1-a816-d608a930855b>
2007-08-08 16:12:33 +0000

    I2O message carrying EmuRUI's vote for first event number to EmuTA.
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@3952 4525493e-7705-40b1-a816-d608a930855b

---
+++ emuDAQ/emuRUI/include/i2oEmuFirstEventNumberMsg.h
@@ -0,0 +1,14 @@
+#ifndef _i2oEmuFirstEventNumber_h_
+#define _i2oEmuFirstEventNumber_h_
+
+#include "i2o.h"
+
+// callback binding (user defined)
+#define I2O_EMU_FIRST_EVENT_NUMBER_CODE 0x0004
+
+typedef struct _I2O_EMU_FIRST_EVENT_NUMBER_MESSAGE_FRAME {
+  I2O_PRIVATE_MESSAGE_FRAME PvtMessageFrame;
+  unsigned long firstEventNumber;
+} I2O_EMU_FIRST_EVENT_NUMBER_MESSAGE_FRAME, *PI2O_EMU_FIRST_EVENT_NUMBER_MESSAGE_FRAME;
+
+#endif
