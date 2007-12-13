1a7f2e7707173fcf8655ad2ee1ab5d7123120884
ichiro <ichiro@4525493e-7705-40b1-a816-d608a930855b>
2007-12-13 16:11:44 +0000

    merged from R2-maint:Release-2_2
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@4152 4525493e-7705-40b1-a816-d608a930855b

--- cscSV/include/CSCSupervisor.h
+++ cscSV/include/CSCSupervisor.h
@@ -164,6 +164,7 @@ private:
 
 	xdaq::ApplicationDescriptor *daq_descr_, *tf_descr_, *ttc_descr_;
 	xoap::MessageReference daq_param_, tf_param_, ttc_param_;
+	bool daq_notavailable_;
 
 	string getDAQMode();
 	string getTFConfig();
@@ -204,9 +205,11 @@ private:
 	class StateTable
 	{
 	public:
-		void addApplication(CSCSupervisor *sv, string klass);
+		StateTable(CSCSupervisor *sv);
+		void addApplication(string klass);
 		void refresh() ;
 		string getState(string klass, unsigned int instance);
+		bool isValidState(string expected);
 		void webOutput(xgi::Output *out, string sv_state)
 				throw (xgi::exception::Exception);
 
