af1454e6de29037b61c6a9e3113b216122164196
gujh <gujh>
2008-02-09 17:34:48 +0000

    OnlineDB

---
+++ emuDCS/OnlineDB/include/BoardsDB.h
@@ -0,0 +1,79 @@
+#ifndef BoardsDB_h
+#define BoardsDB_h
+
+#include <xercesc/dom/DOM.hpp>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+
+class BoardsDB
+{
+public:
+  BoardsDB();
+  ~BoardsDB();
+
+  // General Parser Routines
+  void parseNode(xercesc::DOMNode * pNode); 
+  bool fillInt(std::string item, int & target);
+  bool fillIntX(std::string item, int & target);
+  bool fillLongLongIntX(std::string item, long long int & target);
+  bool fillString(std::string item, std::string & target);
+  bool fillFloat(std::string item, float & target);
+
+  // Parser Routines for RUI-to-chamber_mapping.xml
+  void parseFile(int type,const std::string name);
+  void MainParser(int type,xercesc::DOMNode *pNode);
+  void FillParser(int type,xercesc::DOMNode *pNode);
+  void FillCFEBDB();
+  int CrateToDMBID(std::string crate,int slot);
+  int ChamberToCFEBID(std::string chamber,int cfeb);
+
+  std::string PCBoardsDBXMLFile_;
+  std::string CFEBBoardsDBXMLFile_;
+  std::string CSC2ChamberDBXMLFile_;
+  std::string HOMEDIR_;
+
+
+
+protected:
+
+  int iPCdb;
+  typedef struct pcdb{
+    int slot;
+    std::string crate;
+    std::string type;
+    std::string board;
+  }PCDB;
+  PCDB pcDB[1492];
+
+  int iCFEBdb;
+  typedef struct tcfebdb{
+    int cfebID;
+    std::string csctype;
+    int cscid;
+    int cscnumber;
+  }T_CFEBDB;
+  T_CFEBDB t_cfebdb[2368];
+
+  int iCSCIDdb;
+  typedef struct tcsciddb{
+    std::string csctype;
+    int cscid;
+    std::string chamber;
+  }T_CSCIDDB;
+  T_CSCIDDB t_csciddb[468];
+
+  typedef struct cfebdb{
+    std::string chamber;
+    int cfeb;
+    int cfebID;
+  }CFEBDB;
+  CFEBDB cfebDB[2368];
+
+
+  xercesc::DOMNamedNodeMap * pAttributes_;
+};
+
+
+#endif
--- emuDCS/OnlineDB/include/ConnectionsDB.h
+++ emuDCS/OnlineDB/include/ConnectionsDB.h
@@ -8,7 +8,6 @@
 #include <iomanip>
 
 class ConnectionsDB
-
 {
 public:
   ConnectionsDB();
