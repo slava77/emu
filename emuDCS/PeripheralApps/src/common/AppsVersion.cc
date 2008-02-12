e1e1a3220cb6a32ffe42f323d2d6312ef3aa65e4
liu <liu>
2008-02-12 10:57:24 +0000

    introducing xdaq module version info

---
+++ emuDCS/PeripheralApps/src/common/AppsVersion.cc
@@ -0,0 +1,16 @@
+#include "config/PackageInfo.h"
+
+namespace EmuPeripheralApps {
+	const std::string package  =  "EmuPeripheralApps";
+	const std::string versions =  "4.5";
+	const std::string summary = "emu/emuDCS/PeripheralApps";
+	const std::string description = "Emu Peripheral Crate Application Library";
+	const std::string authors = "Jinghua Liu for cms-emu-onlinesoftware";
+	const std::string link = "http://cms.cern.ch";
+	config::PackageInfo getPackageInfo();
+        void checkPackageDependencies();
+}
+
+GETPACKAGEINFO(EmuPeripheralApps);
+
+void EmuPeripheralApps::checkPackageDependencies() {}
---
+++ emuDCS/PeripheralCore/src/common/CoreVersion.cc
@@ -0,0 +1,16 @@
+#include "config/PackageInfo.h"
+
+namespace EmuPeripheralCore {
+	const std::string package  =  "EmuPeripheralCore";
+	const std::string versions =  "4.5";
+	const std::string summary = "emu/emuDCS/PeripheralCore";
+	const std::string description = "Emu Peripheral Crate Core Library";
+	const std::string authors = "Jinghua Liu for cms-emu-onlinesoftware";
+	const std::string link = "http://cms.cern.ch";
+	config::PackageInfo getPackageInfo();
+        void checkPackageDependencies();
+}
+
+GETPACKAGEINFO(EmuPeripheralCore);
+
+void EmuPeripheralCore::checkPackageDependencies() {}
