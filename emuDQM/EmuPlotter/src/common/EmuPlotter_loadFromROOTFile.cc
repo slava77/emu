c17b28fe3ed60ea5e0a414e566c457ce9670d490
barvic <barvic@4525493e-7705-40b1-a816-d608a930855b>
2008-02-14 16:35:46 +0000

    Separated EmuPlotter from EmuMonitor
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@4295 4525493e-7705-40b1-a816-d608a930855b

---
+++ emuDQM/EmuPlotter/src/common/EmuPlotter_loadFromROOTFile.cc
@@ -0,0 +1,98 @@
+#include "EmuPlotter.h"
+#include "TClass.h"
+
+int EmuPlotter::loadFromROOTFile(std::string rootfile, bool fReset) 
+{
+
+  gSystem->Load("libHistPainter");
+  TFile* rootsrc = TFile::Open( rootfile.c_str());
+  if (!rootsrc) {
+    LOG4CPLUS_ERROR (logger_, "Unable to open " << rootfile.c_str());
+    return -1;
+  }
+  if (!rootsrc->cd("DQMData")) {
+    LOG4CPLUS_ERROR (logger_, "No histos folder in file");
+    return -1;
+  }
+
+  TDirectory *sourcedir = gDirectory;
+
+  TIter nextkey( sourcedir->GetListOfKeys() );
+  TKey *key;
+
+  while ( (key = (TKey*)nextkey())) {
+    TObject *obj = key->ReadObj();
+    if ( obj->IsA()->InheritsFrom( "TDirectory" ) ) 
+      {
+	std::string name=obj->GetName();
+	//      cout << name << endl;
+	std::map<std::string, ME_List >::iterator itr = MEs.find(name);
+	if (name.find("DDU_") != std::string::npos) {
+	  int dduID = -1;
+	  if (sscanf(name.c_str(), "DDU_%d", &dduID) == 1) {
+	    LOG4CPLUS_WARN(logger_,"Found DDU with ID " << dduID);
+	  }
+	  if (MEs.size() == 0 || ((itr = MEs.find(name)) == MEs.end())) {
+	    LOG4CPLUS_WARN(logger_, "List of MEs for " << name << " not found. Booking...");
+	    MEs[name] = bookDDU(dduID);
+	    MECanvases[name] = bookDDUCanvases(dduID);
+	    printMECollection(MEs[name]);
+	  }
+	} else if (name.find("CSC_") != std::string::npos) {
+	  int crate = -1;
+	  int slot = -1;
+	  if (sscanf(name.c_str(), "CSC_%d_%d", &crate, &slot) == 2) {
+	    LOG4CPLUS_WARN(logger_,"Found CSC crate" << crate << "/slot" << slot);
+	  }
+          
+	  int ChamberID     = (((crate) << 4) + slot) & 0xFFF;
+	  // std::cout << ChamberID << std::endl;
+	  if (itr == MEs.end() || (MEs.size()==0)) {
+	    LOG4CPLUS_WARN(logger_, "List of Histos for " << name <<  " not found. Booking...");
+	    MEs[name] = bookChamber(ChamberID);
+	    MECanvases[name] = bookChamberCanvases(ChamberID);
+	    
+	    printMECollection(MEs[name]);
+	  }
+	} else if (name.find("EMU") != std::string::npos) {
+	  int nodeID=0;
+	  /*
+	  int nodeID = -1;
+	  if (sscanf(name.c_str(), "EMU_%d", &nodeID) == 1) {
+	    LOG4CPLUS_WARN(logger_,"Found Global Node with ID " << nodeID);
+	  }
+	  */
+	  if (MEs.size() == 0 || ((itr = MEs.find(name)) == MEs.end())) {
+	    LOG4CPLUS_WARN(logger_, "List of MEs for " << name << " not found. Booking...");
+	    MEs[name] = bookCommon(nodeID);
+	    MECanvases[name] = bookCommonCanvases(nodeID);
+	    printMECollection(MEs[name]);
+	  }
+	}
+      
+	if ((itr = MEs.find(name)) != MEs.end()) {
+	  ME_List_const_iterator h_itr;
+	  sourcedir->cd(name.c_str());
+	  TDirectory* hdir= gDirectory;
+	  for (h_itr = itr->second.begin(); h_itr != itr->second.end(); ++h_itr) {
+	    // h_itr->second->Write();
+	    std::string hname = h_itr->second->getFullName();
+	    TKey *key;
+	    if ( (key = hdir->FindKeyAny(hname.c_str())) != NULL) {
+	      LOG4CPLUS_INFO(logger_, "Found histogram " << hname);
+	      TObject *obj = key->ReadObj();
+	      if ( obj->IsA()->InheritsFrom( "TH1" ) ) {
+		if (fReset) 	h_itr->second->getObject()->Reset();
+	//	h_itr->second->getObject()->Add((TH1*)obj);
+		h_itr->second->setObject((MonitorElement*)obj);
+	      }
+	    } else {
+	      LOG4CPLUS_ERROR(logger_, "Can not find " << hname);
+	    }
+	  }
+	}
+      }
+  }
+
+  return 0;
+}
---
+++ emuDQM/EmuPlotter/src/common/EmuPlotter_saveToROOTFile.cc
@@ -0,0 +1,93 @@
+#include "EmuPlotter.h"
+
+void EmuPlotter::saveToROOTFile(std::string filename)
+{
+  LOG4CPLUS_WARN(logger_, "Saving MEs to ROOT File... " << filename)
+    if (filename == "") {
+      LOG4CPLUS_ERROR(logger_, "Empty ROOT file name ")
+	return;
+	}
+  TFile f(filename.c_str(), "recreate");
+  if (!f.IsZombie()) {
+    fBusy = true;
+    gStyle->SetPalette(1,0);
+    f.cd();
+    std::map<std::string, ME_List>::iterator itr;
+    ME_List_const_iterator h_itr;
+
+//     TDirectory * hdir = f.mkdir("histos");
+    TDirectory * hdir = f.mkdir("DQMData");
+    hdir->cd();
+    for (itr = MEs.begin(); itr != MEs.end(); ++itr) {
+      TDirectory * rdir = hdir->mkdir((itr->first).c_str());
+      rdir->cd();
+      for (h_itr = itr->second.begin(); h_itr != itr->second.end(); ++h_itr) {
+//	h_itr->second->Draw();	
+	h_itr->second->Write();
+      }
+      hdir->cd();
+    } 
+
+    /*
+      TDirectory* rdir = f.mkdir("Common");
+      rdir->cd();
+      for (h_itr = commonMEfactory.begin(); h_itr != commonMEfactory.end(); ++h_itr) {
+      h_itr->second->Write();
+      }
+      f.cd();
+    */
+    f.Close();
+    LOG4CPLUS_WARN(logger_, "Done.");
+  //  saveCanvasesToROOTFile(filename);
+    fBusy=false;
+  } else {
+    LOG4CPLUS_ERROR(logger_, "Unable to open output ROOT file " << filename);
+  }
+}
+
+void EmuPlotter::saveCanvasesToROOTFile(std::string filename)
+{
+  LOG4CPLUS_WARN(logger_, "Saving Canvases to ROOT File... " << filename)
+    if (filename == "") {
+      LOG4CPLUS_ERROR(logger_, "Empty ROOT file name ")
+	return;
+	}
+
+  
+  TFile f(filename.c_str(), "update");
+  if (!f.IsZombie()) {
+    fBusy=true;
+    gStyle->SetPalette(1,0);
+    f.cd(); 
+    std::map<std::string, ME_List>::iterator me_itr;
+    std::map<std::string, MECanvases_List>::iterator itr;
+
+    MECanvases_List_const_iterator h_itr;
+  
+
+    for (itr = MECanvases.begin(); itr != MECanvases.end(); ++itr) {
+      TDirectory * rdir = f.mkdir((itr->first).c_str());
+      rdir->cd();
+      for (h_itr = itr->second.begin(); h_itr != itr->second.end(); ++h_itr) {
+	me_itr = MEs.find(itr->first);
+	std::string folder = h_itr->second->getFolder();
+	if (!rdir->GetDirectory(folder.c_str(),false)) {
+	  TDirectory *subdir = rdir->mkdir(folder.c_str());
+	  subdir->cd();
+	} else rdir->cd(folder.c_str());
+	if (me_itr != MEs.end()) {
+	  h_itr->second->Draw(me_itr->second);
+	  h_itr->second->Write();
+	}
+      }
+      f.cd();
+    }
+    f.Close();
+    LOG4CPLUS_WARN(logger_, "Done.");
+    fBusy=false;
+  } else {
+    LOG4CPLUS_ERROR(logger_, "Unable to open output ROOT file " << filename);
+  }
+}
+
+
