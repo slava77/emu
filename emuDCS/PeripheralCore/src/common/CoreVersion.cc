7fca5a52e9fc6430687db73098a444131a6a70ac
liu <liu>
2008-06-24 11:22:21 +0000

    bump vesion to 6.1

--- emuDCS/PeripheralCore/src/common/CoreVersion.cc
+++ emuDCS/PeripheralCore/src/common/CoreVersion.cc
@@ -2,7 +2,7 @@
 
 namespace EmuPeripheralCore {
 	const std::string package  =  "EmuPeripheralCore";
-	const std::string versions =  "6.0";
+	const std::string versions =  "6.1";
 	const std::string summary = "emu/emuDCS/PeripheralCore";
 	const std::string description = "Emu Peripheral Crate Core Library";
 	const std::string authors = "Jinghua Liu for cms-emu-onlinesoftware";
