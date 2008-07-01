dca063728fe74c52ee137456e212877f8b3ba684
kkotov <kkotov@4525493e-7705-40b1-a816-d608a930855b>
2008-07-01 08:49:19 +0000

    Reduced binning to fight memory consumption
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@4722 4525493e-7705-40b1-a816-d608a930855b

--- emuDQM/EmuTFMonitor/test/common/EmuTFtest.cpp
+++ emuDQM/EmuTFMonitor/test/common/EmuTFtest.cpp
@@ -21,10 +21,11 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <time.h>
+#include <fstream>
 
 //#define BASE "/nfshome0/cscdqm/TriDAS/emu/emuDQM/"
 
-//To compile on lxplus:   g++ -o test EmuTFtest.cpp -I/afs/cern.ch/cms/Releases/XDAQ/XDAQ_3_4/daq/extern/xerces/linuxx86/include/ -L/afs/cern.ch/cms/Releases/XDAQ/XDAQ_3_4/daq/extern/xerces/linuxx86/lib/ -lxerces-c `root-config --cflags` `root-config --glibs` -Wall -I/afs/cern.ch/user/k/kkotov/CMSSW_0_8_0_pre2/src/ -I../../src/common/ -I../../include/
+//To compile on lxplus:  /afs/cern.ch/cms/sw/slc4_ia32_gcc345/external/gcc/3.4.5-CMS8/bin/g++ -o test EmuTFtest.cpp -I/afs/cern.ch/cms/Releases/XDAQ/XDAQ_3_5/daq/extern/xerces/linuxx86/include/ -L/usr/lib64/ -L/afs/cern.ch/cms/Releases/XDAQ/XDAQ_3_5/daq/extern/xerces/linuxx86/lib/ -lxerces-c `root-config --cflags` `root-config --glibs` -Wall -I/afs/cern.ch/user/k/kkotov/CMSSW_2_0_7/src/ -I../../src/common/ -I../../include/ -DBASE='"/afs/cern.ch/user/k/kkotov/TFDQM/TriDAS/emu/emuDQM/"' -Xlinker -rpath -Xlinker /afs/cern.ch/cms/sw/slc4_ia32_gcc345/lcg/root/5.18.00a-cms7//lib 
 //To compile on cmsdaq03: g++ -o test EmuTFtest.cpp -I$XDAQ_ROOT/daq/extern/xerces/linuxx86/include/ -L$XDAQ_ROOT/daq/extern/xerces/linuxx86/lib/ -lxerces-c `root-config --cflags` `root-config --glibs` -Wall -I/home/daq/kotov/CMSSW_0_5_1/src/ -I../../src/common/ -I../../include/
 //To compile on slice11:  g++ -o test EmuTFtest.cpp -I/home/slice/DAQKit/v3.6/TriDAS/daq/extern/xerces/linuxx86/include/ -I../../src/common/ -I../../include/ -L/home/slice/DAQKit/v3.6/TriDAS/daq/extern/xerces/linuxx86/lib/ -lxerces-c `root-config --cflags` `root-config --glibs` -Wall -I/home/slice/kotov/CMSSW_0_8_0_pre2/src/
 //To compile on emudqm: g++ -o test EmuTFtest.cpp -I/home/cscdqm/DAQKit/v4.2.1/TriDAS/daq/extern/xerces/linuxx86/include/ -I../../src/common/ -I../../include/ -L/home/cscdqm/DAQKit/v4.2.1/TriDAS/daq/extern/xerces/linuxx86/lib/ -lxerces-c `root-config --cflags` `root-config --glibs` -Wall -I/home/cscdqm/CMSSW_1_3_0_pre4/src
@@ -41,7 +42,6 @@ int main(int argc, char *argv[]){
 	int a=2;
 	char *v[]={"qwe","-b"};
 	TApplication app("qwe", &a, v);
-
 	// Histograms/Canvases description
 	std::map<std::string,HistAttributes>   histList   = parseHistXML  (BASE "EmuTFMonitor/xml/CSCTF_histograms.xml");
 	std::map<std::string,CanvasAttributes> canvasList = parseCanvasXML(BASE "EmuTFMonitor/xml/CSCTF_canvases.xml");
@@ -117,122 +117,152 @@ int main(int argc, char *argv[]){
 //	map<string,string> alias;
 //	alias[""] = "";
 
+	ofstream report("report.txt");
+	report<<str.str()<<ends;
+	report.close();
+
+std::ostringstream str2;
+for(int spNum=1; spNum<=12; spNum++){
+	string sp="SP";
+	switch ( spNum ){
+		case 1: sp+="1/"; break;
+		case 2: sp+="2/"; break;
+		case 3: sp+="3/"; break;
+		case 4: sp+="4/"; break;
+		case 5: sp+="5/"; break;
+		case 6: sp+="6/"; break;
+		case 7: sp+="7/"; break;
+		case 8: sp+="8/"; break;
+		case 9: sp+="9/"; break;
+		case 10:sp+="10/";break;
+		case 11:sp+="11/";break;
+		case 12:sp+="12/";break;
+	}
+	str2<<sp<<endl;
+
 	map<string,string> neighbour;
-	neighbour["F2/CSC4"] = "F3/CSC7";
-	neighbour["F2/CSC5"] = "F3/CSC8";
-	neighbour["F2/CSC6"] = "F3/CSC9";
-	neighbour["F2/CSC7"] = "F3/CSC7";
-	neighbour["F2/CSC8"] = "F3/CSC8";
-	neighbour["F2/CSC9"] = "F3/CSC9";
+	neighbour[sp+"F2/CSC4"] = sp+"F3/CSC7";
+	neighbour[sp+"F2/CSC5"] = sp+"F3/CSC8";
+	neighbour[sp+"F2/CSC6"] = sp+"F3/CSC9";
+	neighbour[sp+"F2/CSC7"] = sp+"F3/CSC7";
+	neighbour[sp+"F2/CSC8"] = sp+"F3/CSC8";
+	neighbour[sp+"F2/CSC9"] = sp+"F3/CSC9";
 
-	neighbour["F1/CSC4"] = "F3/CSC4";
-	neighbour["F1/CSC5"] = "F3/CSC5";
-	neighbour["F1/CSC6"] = "F3/CSC6";
-	neighbour["F1/CSC7"] = "F3/CSC4";
-	neighbour["F1/CSC8"] = "F3/CSC5";
-	neighbour["F1/CSC9"] = "F3/CSC6";
+	neighbour[sp+"F1/CSC4"] = sp+"F3/CSC4";
+	neighbour[sp+"F1/CSC5"] = sp+"F3/CSC5";
+	neighbour[sp+"F1/CSC6"] = sp+"F3/CSC6";
+	neighbour[sp+"F1/CSC7"] = sp+"F3/CSC4";
+	neighbour[sp+"F1/CSC8"] = sp+"F3/CSC5";
+	neighbour[sp+"F1/CSC9"] = sp+"F3/CSC6";
 
-	neighbour["F4/CSC4"] = "F3/CSC4";
-	neighbour["F4/CSC5"] = "F3/CSC5";
-	neighbour["F4/CSC6"] = "F3/CSC6";
-	neighbour["F4/CSC7"] = "F3/CSC7";
-	neighbour["F4/CSC8"] = "F3/CSC8";
-	neighbour["F4/CSC9"] = "F3/CSC9";
+	neighbour[sp+"F4/CSC4"] = sp+"F3/CSC4";
+	neighbour[sp+"F4/CSC5"] = sp+"F3/CSC5";
+	neighbour[sp+"F4/CSC6"] = sp+"F3/CSC6";
+	neighbour[sp+"F4/CSC7"] = sp+"F3/CSC7";
+	neighbour[sp+"F4/CSC8"] = sp+"F3/CSC8";
+	neighbour[sp+"F4/CSC9"] = sp+"F3/CSC9";
 
-	neighbour["F4/CSC1"] = "F3/CSC1";
-	neighbour["F4/CSC2"] = "F3/CSC2";
-	neighbour["F4/CSC3"] = "F3/CSC3";
+	neighbour[sp+"F4/CSC1"] = sp+"F3/CSC1";
+	neighbour[sp+"F4/CSC2"] = sp+"F3/CSC2";
+	neighbour[sp+"F4/CSC3"] = sp+"F3/CSC3";
 
-	neighbour["F1/CSC4"] = "F3/CSC4";
-	neighbour["F1/CSC5"] = "F3/CSC5";
-	neighbour["F1/CSC6"] = "F3/CSC6";
+	neighbour[sp+"F5/CSC1"] = sp+"F4/CSC1";
+	neighbour[sp+"F5/CSC2"] = sp+"F4/CSC2";
+	neighbour[sp+"F5/CSC3"] = sp+"F4/CSC3";
 
-	neighbour["F2/CSC4"] = "F3/CSC7";
-	neighbour["F2/CSC5"] = "F3/CSC8";
-	neighbour["F2/CSC6"] = "F3/CSC9";
+	neighbour[sp+"F1/CSC4"] = sp+"F3/CSC4";
+	neighbour[sp+"F1/CSC5"] = sp+"F3/CSC5";
+	neighbour[sp+"F1/CSC6"] = sp+"F3/CSC6";
 
-	neighbour["F1/CSC7"] = "F3/CSC4";
-	neighbour["F1/CSC8"] = "F3/CSC5";
-	neighbour["F1/CSC9"] = "F3/CSC6";
+	neighbour[sp+"F2/CSC4"] = sp+"F3/CSC7";
+	neighbour[sp+"F2/CSC5"] = sp+"F3/CSC8";
+	neighbour[sp+"F2/CSC6"] = sp+"F3/CSC9";
 
-	neighbour["F2/CSC7"] = "F3/CSC7";
-	neighbour["F2/CSC8"] = "F3/CSC8";
-	neighbour["F2/CSC9"] = "F3/CSC9";
+	neighbour[sp+"F1/CSC7"] = sp+"F3/CSC4";
+	neighbour[sp+"F1/CSC8"] = sp+"F3/CSC5";
+	neighbour[sp+"F1/CSC9"] = sp+"F3/CSC6";
 
-	neighbour["F1/CSC1"] = "F3/CSC1";
-	neighbour["F1/CSC2"] = "F3/CSC2";
-	neighbour["F1/CSC3"] = "F3/CSC2";
+	neighbour[sp+"F2/CSC7"] = sp+"F3/CSC7";
+	neighbour[sp+"F2/CSC8"] = sp+"F3/CSC8";
+	neighbour[sp+"F2/CSC9"] = sp+"F3/CSC9";
 
-	neighbour["F2/CSC1"] = "F3/CSC2";
-	neighbour["F2/CSC2"] = "F3/CSC2";
-	neighbour["F2/CSC3"] = "F3/CSC3";
+	neighbour[sp+"F1/CSC1"] = sp+"F3/CSC1";
+	neighbour[sp+"F1/CSC2"] = sp+"F3/CSC2";
+	neighbour[sp+"F1/CSC3"] = sp+"F3/CSC2";
 
-	str<<endl<<"Timing for neighbour chambers: "<<endl<<endl;
-	for(int mpc=1; mpc<5; mpc++){
+	neighbour[sp+"F2/CSC1"] = sp+"F3/CSC2";
+	neighbour[sp+"F2/CSC2"] = sp+"F3/CSC2";
+	neighbour[sp+"F2/CSC3"] = sp+"F3/CSC3";
+
+	str2<<endl<<"Timing for neighbour chambers (SP"<<spNum<<"): "<<endl<<endl;
+	for(int mpc=1; mpc<=5; mpc++){
 		for(int csc=1; csc<10; csc++){
+			if(mpc==5 && csc>3) continue;
 			ostringstream label;
-			label<<"F"<<mpc<<"/CSC"<<csc;
-			TH2F *timing = (TH2F*)filler.bookkeeper().get("csc_timing",1,mpc,csc);
-			if( !timing ) continue;
+			label<<(sp+"F")<<mpc<<"/CSC"<<csc;
+			TH2F *timing = (TH2F*)filler.bookkeeper().get("csc_timing",spNum,mpc,csc);
+			if( !timing ){ std::cout<<label.str()<<"/"<<"csc_timing not booked"<<std::endl; continue; }
 			for(int yBin=1; yBin<timing->GetNbinsY(); yBin++){
-				if( neighbour[label.str()] != timing->GetYaxis()->GetBinLabel(yBin) ) continue;
+				if( neighbour[label.str()] != (sp+timing->GetYaxis()->GetBinLabel(yBin)) ) continue;
 				double m0=0, m1=0, m2=0;
 				for(int xBin=1; xBin<timing->GetNbinsX(); xBin++){
 					m0 += timing->GetBinContent(xBin,yBin);
 					m1 += timing->GetBinContent(xBin,yBin)*(xBin-8);
 					m2 += timing->GetBinContent(xBin,yBin)*(xBin-8)*(xBin-8);
-
 				}
-				str<<label.str()<<" - "<<neighbour[label.str()]<<(m1>0?" = +":" = ")<<m1/m0<<" RMS = "<<sqrt(m2/m0-m1*m1/m0/m0)<<" nHits = "<<m0<<endl;
+				str2<<label.str()<<" - "<<neighbour[label.str()]<<(m1>0?" = +":" = ")<<m1/m0<<" RMS = "<<sqrt(m2/m0-m1*m1/m0/m0)<<" nHits = "<<m0<<endl;
 			}
-
 		}
 	}
 
 	map<string,string> overlap;
-	overlap["F4/CSC4"] = "F4/CSC5";
-	overlap["F4/CSC5"] = "F4/CSC6";
-	overlap["F4/CSC6"] = "F4/CSC7";
-	overlap["F4/CSC7"] = "F4/CSC8";
-	overlap["F4/CSC8"] = "F4/CSC9";
+	overlap[sp+"F4/CSC4"] = sp+"F4/CSC5";
+	overlap[sp+"F4/CSC5"] = sp+"F4/CSC6";
+	overlap[sp+"F4/CSC6"] = sp+"F4/CSC7";
+	overlap[sp+"F4/CSC7"] = sp+"F4/CSC8";
+	overlap[sp+"F4/CSC8"] = sp+"F4/CSC9";
 
-	overlap["F3/CSC4"] = "F3/CSC5";
-	overlap["F3/CSC5"] = "F3/CSC6";
-	overlap["F3/CSC6"] = "F3/CSC7";
-	overlap["F3/CSC7"] = "F3/CSC8";
-	overlap["F3/CSC8"] = "F3/CSC9";
+	overlap[sp+"F3/CSC4"] = sp+"F3/CSC5";
+	overlap[sp+"F3/CSC5"] = sp+"F3/CSC6";
+	overlap[sp+"F3/CSC6"] = sp+"F3/CSC7";
+	overlap[sp+"F3/CSC7"] = sp+"F3/CSC8";
+	overlap[sp+"F3/CSC8"] = sp+"F3/CSC9";
 
-	overlap["F4/CSC1"] = "F4/CSC2";
-	overlap["F4/CSC2"] = "F4/CSC3";
+	overlap[sp+"F5/CSC1"] = sp+"F5/CSC2";
+	overlap[sp+"F5/CSC2"] = sp+"F5/CSC3";
 
-	overlap["F3/CSC1"] = "F3/CSC2";
-	overlap["F3/CSC2"] = "F3/CSC3";
+	overlap[sp+"F4/CSC1"] = sp+"F4/CSC2";
+	overlap[sp+"F4/CSC2"] = sp+"F4/CSC3";
 
-	overlap["F1/CSC1"] = "F1/CSC2";
-	overlap["F1/CSC2"] = "F1/CSC3";
-	overlap["F1/CSC3"] = "F2/CSC1";
-	overlap["F1/CSC4"] = "F1/CSC5";
-	overlap["F1/CSC5"] = "F1/CSC6";
-	overlap["F1/CSC6"] = "F2/CSC4";
-	overlap["F1/CSC7"] = "F1/CSC8";
-	overlap["F1/CSC8"] = "F1/CSC9";
+	overlap[sp+"F3/CSC1"] = sp+"F3/CSC2";
+	overlap[sp+"F3/CSC2"] = sp+"F3/CSC3";
 
-	overlap["F2/CSC1"] = "F2/CSC2";
-	overlap["F2/CSC2"] = "F2/CSC3";
-	overlap["F2/CSC4"] = "F2/CSC5";
-	overlap["F2/CSC5"] = "F2/CSC6";
-	overlap["F2/CSC7"] = "F2/CSC8";
-	overlap["F2/CSC8"] = "F2/CSC9";
+	overlap[sp+"F1/CSC1"] = sp+"F1/CSC2";
+	overlap[sp+"F1/CSC2"] = sp+"F1/CSC3";
+	overlap[sp+"F1/CSC3"] = sp+"F2/CSC1";
+	overlap[sp+"F1/CSC4"] = sp+"F1/CSC5";
+	overlap[sp+"F1/CSC5"] = sp+"F1/CSC6";
+	overlap[sp+"F1/CSC6"] = sp+"F2/CSC4";
+	overlap[sp+"F1/CSC7"] = sp+"F1/CSC8";
+	overlap[sp+"F1/CSC8"] = sp+"F1/CSC9";
 
-	str<<endl<<"Timing for overlapping chambers: "<<endl<<endl;
-	for(int mpc=1; mpc<5; mpc++){
+	overlap[sp+"F2/CSC1"] = sp+"F2/CSC2";
+	overlap[sp+"F2/CSC2"] = sp+"F2/CSC3";
+	overlap[sp+"F2/CSC4"] = sp+"F2/CSC5";
+	overlap[sp+"F2/CSC5"] = sp+"F2/CSC6";
+	overlap[sp+"F2/CSC7"] = sp+"F2/CSC8";
+	overlap[sp+"F2/CSC8"] = sp+"F2/CSC9";
+
+	str2<<endl<<"Timing for overlapping chambers (SP"<<spNum<<"): "<<endl<<endl;
+	for(int mpc=1; mpc<=5; mpc++){
 		for(int csc=1; csc<10; csc++){
+			if(mpc==5 && csc>3) continue;
 			ostringstream label;
-			label<<"F"<<mpc<<"/CSC"<<csc;
-			TH2F *timing = (TH2F*)filler.bookkeeper().get("csc_timing",1,mpc,csc);
-			if( !timing ) continue;
+			label<<(sp+"F")<<mpc<<"/CSC"<<csc;
+			TH2F *timing = (TH2F*)filler.bookkeeper().get("csc_timing",spNum,mpc,csc);
+			if( !timing ){ std::cout<<label.str()<<"/"<<"csc_timing not booked"<<std::endl; continue; }
 			for(int yBin=1; yBin<=timing->GetNbinsY(); yBin++){
-				if( overlap[label.str()] != timing->GetYaxis()->GetBinLabel(yBin) ) continue;
+				if( overlap[label.str()] != (sp+timing->GetYaxis()->GetBinLabel(yBin)) ) continue;
 				double m0=0, m1=0, m2=0;
 				for(int xBin=1; xBin<timing->GetNbinsX(); xBin++){
 					m0 += timing->GetBinContent(xBin,yBin);
@@ -240,12 +270,17 @@ int main(int argc, char *argv[]){
 					m2 += timing->GetBinContent(xBin,yBin)*(xBin-8)*(xBin-8);
 
 				}
-				str<<label.str()<<" - "<<overlap[label.str()]<<(m1>0?" = +":" = ")<<m1/m0<<" RMS = "<<sqrt(m2/m0-m1*m1/m0/m0)<<" nHits = "<<m0<<endl;
+				str2<<label.str()<<" - "<<overlap[label.str()]<<(m1>0?" = +":" = ")<<m1/m0<<" RMS = "<<sqrt(m2/m0-m1*m1/m0/m0)<<" nHits = "<<m0<<endl;
 			}
 
 		}
 	}
-	str<<endl;
+	str2<<endl;
+}
+
+	ofstream report2("timing.txt");
+	report2<<str2.str()<<ends;
+	report2.close();
 
 	std::map<std::string,TCanvas*> rootCanvases = filler.bookkeeper().wrapToCanvases(canvasList);
 	std::map<std::string,TCanvas*>::iterator iter = rootCanvases.begin();
@@ -255,14 +290,13 @@ int main(int argc, char *argv[]){
 		iter->second->Write();
 		iter++;
 	}
-	// Dump summary information
-	string sum = str.str();
+	// Write summary information
+	string sum = str.str() + str2.str();
 	TH1C summary("summary","Summary",sum.length(),0,1);
 	for(unsigned int i=0;i<sum.length();i++) summary.SetBinContent(i+1,sum[i]);
 	cout<<"Summary ["<<summary.GetNbinsX()<<"="<<sum.length()<<"]:"<<endl<<(summary.GetArray()+1)<<endl;
 	summary.Write();
 	file.Close();
-
 	app.Terminate();
 	return 0;
 }
