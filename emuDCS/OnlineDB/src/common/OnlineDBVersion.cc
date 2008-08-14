6279f275c9a108882e5354a191adfbc92193f834
geurts <geurts>
2008-08-14 09:28:23 +0000

    introduce default (xdaq) package information

---
+++ emuDCS/OnlineDB/src/common/OnlineDBVersion.cc
@@ -0,0 +1,16 @@
+#include "config/PackageInfo.h"
+
+namespace EmuOnlineDB {
+        const std::string package  =  "EmuOnlineDB";
+        const std::string versions =  "6.4";
+        const std::string summary = "emu/emuDCS/OnlineDB";
+        const std::string description = "Emu Online Database Library";
+        const std::string authors = "Stan Durkin";
+        const std::string link = "http://cern.ch/cscdoc";
+        config::PackageInfo getPackageInfo();
+        void checkPackageDependencies();
+}
+
+GETPACKAGEINFO(EmuOnlineDB);
+
+void EmuOnlineDB::checkPackageDependencies() {}
