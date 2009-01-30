f3aee9d0410cf02fc97f1e06d427d4c70f1ea19f
banicz <banicz@4525493e-7705-40b1-a816-d608a930855b>
2009-01-30 16:36:02 +0000

    Emu DAQ server classes
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@5157 4525493e-7705-40b1-a816-d608a930855b

---
+++ daq/server/include/emu/daq/server/version.h
@@ -0,0 +1,21 @@
+#ifndef __emu_server_version_h__
+#define __emu_server_version_h__
+
+#include "config/PackageInfo.h"
+
+namespace emudaqserver {
+	const std::string package     = "emudaqserver";
+	const std::string versions    = "09.01.30";
+	const std::string description = "Emu (CSC) DAQ servers";
+
+	const std::string summary     = "emu/daq/server";
+	const std::string authors     = "Karoly Banicz";
+	const std::string link        = "";
+
+	config::PackageInfo getPackageInfo();
+	void checkPackageDependencies()
+			throw (config::PackageInfo::VersionException);
+	std::set<std::string, std::less<std::string> > getPackageDependencies();
+}
+
+#endif
