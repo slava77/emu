5ff957fd581352d14e60f5fd80c1c73367ad60e5
banicz <banicz>
2009-01-30 15:39:21 +0000

    base class for emu applications under direct supervisor control

---
+++ base/include/emu/base/Supervised.h
@@ -0,0 +1,37 @@
+#ifndef __SUPERVISED_H__
+#define __SUPERVISED_H__
+
+#include "xdaq/WebApplication.h"
+
+#include <string>
+#include "xdata/String.h"
+#include "toolbox/fsm/FiniteStateMachine.h"
+
+namespace emu { namespace base {
+
+class Supervised : public xdaq::WebApplication  
+{
+public:
+	XDAQ_INSTANTIATOR();
+
+	Supervised(xdaq::ApplicationStub *stub)
+	  throw (xdaq::exception::Exception);
+
+protected:
+	void stateChanged(toolbox::fsm::FiniteStateMachine &fsm)
+			throw (toolbox::fsm::exception::Exception);
+	void transitionFailed(toolbox::Event::Reference event)
+			throw (toolbox::fsm::exception::Exception);
+	void fireEvent(std::string event) throw (toolbox::fsm::exception::Exception);
+
+	xoap::MessageReference createReply(xoap::MessageReference message)
+			throw (xoap::exception::Exception);
+
+	toolbox::fsm::FiniteStateMachine fsm_;
+	xdata::String state_;
+};
+
+}} // namespace emu::base
+
+#endif  // ifndef __SUPERVISED_H__
+// vim: set ai sw=4 ts=4:
---
+++ base/include/emu/base/version.h
@@ -0,0 +1,22 @@
+#ifndef __emu_base_version_h__
+#define __emu_base_version_h__
+
+#include "config/PackageInfo.h"
+
+namespace emubase {
+	const std::string package     = "emubase";
+	const std::string versions    = "07.00.00";
+	const std::string description = "Emu (CSC) applications' base classes";
+
+	const std::string summary     = "emu/base";
+	const std::string authors     = "Ichiro Suzuki";
+	const std::string link        = "http://localhost:20000/";
+
+	config::PackageInfo getPackageInfo();
+	void checkPackageDependencies()
+			throw (config::PackageInfo::VersionException);
+	std::set<std::string, std::less<std::string> > getPackageDependencies();
+}
+
+#endif  // ifndef __EMU_BASE_VERSION_H__
+
