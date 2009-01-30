9d379d1c1c6d106ea85adda0d5b5828b919ca602
banicz <banicz>
2009-01-30 16:34:29 +0000

    Emu RUI is now called emu::daq::rui::Application

---
+++ daq/rui/include/emu/daq/rui/version.h
@@ -0,0 +1,22 @@
+#ifndef __emu_daq_rui_version_h__
+#define __emu_daq_rui_version_h__
+
+#include "config/PackageInfo.h"
+
+namespace emudaqrui {
+	const std::string package     = "emudaqrui";
+	const std::string versions    = "09.01.30";
+	const std::string description = "Emu (CSC) Readout Unit Input";
+
+	const std::string summary     = "emu/daq/rui";
+	const std::string authors     = "Steve Murray, Karoly Banicz";
+	const std::string link        = "";
+
+	config::PackageInfo getPackageInfo();
+	void checkPackageDependencies()
+			throw (config::PackageInfo::VersionException);
+	std::set<std::string, std::less<std::string> > getPackageDependencies();
+}
+
+#endif
+
---
+++ daq/rui/src/common/version.cc
@@ -0,0 +1,34 @@
+//
+// version.cc
+
+#include "emu/daq/rui/version.h"
+
+#include "toolbox/version.h"
+#include "xoap/version.h"
+#include "xdata/version.h"
+#include "xdaq/version.h"
+
+using namespace std;
+
+GETPACKAGEINFO(emudaqrui)
+
+void emudaqrui::checkPackageDependencies()
+		throw (config::PackageInfo::VersionException)
+{
+	CHECKDEPENDENCY(toolbox)
+	CHECKDEPENDENCY(xoap)
+	CHECKDEPENDENCY(xdata)
+	CHECKDEPENDENCY(xdaq)
+}
+
+set<string, less<string> > xdaq::getPackageDependencies()
+{
+	set<string, less<string> > dependencies;
+
+	ADDDEPENDENCY(dependencies, toolbox);
+	ADDDEPENDENCY(dependencies, xoap);
+	ADDDEPENDENCY(dependencies, xdata);
+	ADDDEPENDENCY(dependencies, xdaq);
+
+	return dependencies;
+}
