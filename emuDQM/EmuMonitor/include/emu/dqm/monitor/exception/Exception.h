5bdeed4341046d3e982e942cbfb10d3baef42fc7
barvic <barvic>
2009-02-26 15:30:38 +0000

    Exception.h fixes for compatibility with XDAQ6

--- emuDQM/EmuMonitor/include/emu/dqm/monitor/exception/Exception.h
+++ emuDQM/EmuMonitor/include/emu/dqm/monitor/exception/Exception.h
@@ -2,6 +2,7 @@
 #define _emu_dqm_monitor_exception_Exception_h_
 
 #include "xcept/Exception.h"
+#include "xdaq/version.h"
 
 /**
  * Exceptions thrown by the  package in the
@@ -10,17 +11,83 @@
 
 namespace emu { namespace dqm { 
 
+  /** Check if XDAQ version is less than XDAQ7 */
+#if (XDAQ_VERSION_MAJOR <= 3) && (XDAQ_VERSION_MINOR < 24)
+
+  namespace monitor
+    {
+      namespace exception
+	{
+	  /**
+	   * Exception raised by the package.
+	   */
+	  class Exception: public xcept::Exception
+	    {
+            public:
+
+	      Exception(std::string name,
+			std::string message,
+			std::string module,
+			int line,
+			std::string function)
+		: xcept::Exception(name, message, module, line, function)
+		{
+		}
+
+	      Exception(std::string name,
+			std::string message,
+			std::string module,
+			int line,
+			std::string function,
+			xcept::Exception & e)
+		: xcept::Exception(name, message, module, line, function, e)
+		{
+		}
+	    };
+
+	  class I2O: public xcept::Exception
+	    {
+            public:
+
+	      Exception(std::string name,
+			std::string message,
+			std::string module,
+			int line,
+			std::string function)
+		: xcept::Exception(name, message, module, line, function)
+		{
+		}
+
+	      Exception(std::string name,
+			std::string message,
+			std::string module,
+			int line,
+			std::string function,
+			xcept::Exception & e)
+		: xcept::Exception(name, message, module, line, function, e)
+		{
+		}
+	    };
+
+
+	}
+    }
+
+#else
+
   /**
    * Generic exception raised by the emu::daq::rui package.
    */
   XCEPT_DEFINE_EXCEPTION(monitor, Exception)
 
-  /**
-   * I2O exception raised by the emu::daq::rui package.
-   */
+    /**
+     * I2O exception raised by the emu::daq::rui package.
+     */
   XCEPT_DEFINE_EXCEPTION(monitor, I2O)
 
-}}
+#endif
+
+    }}
 
 #endif
 
