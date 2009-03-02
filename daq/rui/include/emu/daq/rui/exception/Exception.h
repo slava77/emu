24fe00953dec31b5e8df8b1772f287d57787553d
banicz <banicz@4525493e-7705-40b1-a816-d608a930855b>
2009-03-02 12:38:04 +0000

    Undo definition by macro only available in XDAQr7.
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@5204 4525493e-7705-40b1-a816-d608a930855b

--- daq/rui/include/emu/daq/rui/exception/Exception.h
+++ daq/rui/include/emu/daq/rui/exception/Exception.h
@@ -13,12 +13,42 @@ namespace emu { namespace daq {
   /**
    * Generic exception raised by the emu::daq::rui package.
    */
-  XCEPT_DEFINE_EXCEPTION(rui, Exception)
+//   XCEPT_DEFINE_EXCEPTION(rui, Exception)
 
   /**
    * I2O exception raised by the emu::daq::rui package.
    */
-  XCEPT_DEFINE_EXCEPTION(rui, I2O)
+//   XCEPT_DEFINE_EXCEPTION(rui, I2O)
+
+// TODO: revert to definition by macro
+
+  namespace rui{
+  namespace exception{
+  class Exception: public xcept::Exception
+  {
+  public:
+
+    Exception(std::string name,
+	      std::string message,
+	      std::string module,
+	      int line,
+	      std::string function)
+      : xcept::Exception(name, message, module, line, function)
+    {
+    }
+
+    Exception(std::string name,
+	      std::string message,
+	      std::string module,
+	      int line,
+	      std::string function,
+	      xcept::Exception & e)
+      : xcept::Exception(name, message, module, line, function, e)
+    {
+    }
+  };
+  }
+  }
 
 }}
 
