97503b220a12c87cb3bccbf63e3cc2256034869f
banicz <banicz>
2009-03-02 12:38:04 +0000

    Undo definition by macro only available in XDAQr7.

--- daq/fu/include/emu/daq/fu/exception/Exception.h
+++ daq/fu/include/emu/daq/fu/exception/Exception.h
@@ -13,12 +13,42 @@ namespace emu { namespace daq {
   /**
    * Generic exception raised by the emu::daq::fu package.
    */
-  XCEPT_DEFINE_EXCEPTION(fu, Exception)
+//   XCEPT_DEFINE_EXCEPTION(fu, Exception)
 
   /**
    * I2O exception raised by the emu::daq::fu package.
    */
-  XCEPT_DEFINE_EXCEPTION(fu, I2O)
+//   XCEPT_DEFINE_EXCEPTION(fu, I2O)
+
+// TODO: revert to definition by macro
+
+  namespace fu{
+    namespace exception{
+        class Exception: public xcept::Exception
+        {
+            public:
+
+            Exception(std::string name,
+                      std::string message,
+                      std::string module,
+                      int line,
+                      std::string function)
+            : xcept::Exception(name, message, module, line, function)
+            {
+            }
+
+            Exception(std::string name,
+                      std::string message,
+                      std::string module,
+                      int line,
+                      std::string function,
+                      xcept::Exception & e)
+            : xcept::Exception(name, message, module, line, function, e)
+            {
+            }
+        };
+    }
+  }
 
 }}
 
--- daq/manager/include/emu/daq/manager/exception/Exception.h
+++ daq/manager/include/emu/daq/manager/exception/Exception.h
@@ -13,7 +13,37 @@ namespace emu { namespace daq {
   /**
    * Generic exception raised by the emu::daq::manager package.
    */
-  XCEPT_DEFINE_EXCEPTION(manager, Exception)
+//   XCEPT_DEFINE_EXCEPTION(manager, Exception)
+
+// TODO: revert to definition by macro
+
+  namespace manager{
+    namespace exception{
+        class Exception: public xcept::Exception
+        {
+            public:
+
+            Exception(std::string name,
+                      std::string message,
+                      std::string module,
+                      int line,
+                      std::string function)
+            : xcept::Exception(name, message, module, line, function)
+            {
+            }
+
+            Exception(std::string name,
+                      std::string message,
+                      std::string module,
+                      int line,
+                      std::string function,
+                      xcept::Exception & e)
+            : xcept::Exception(name, message, module, line, function, e)
+            {
+            }
+        };
+    }
+  }
 
 }}
 
