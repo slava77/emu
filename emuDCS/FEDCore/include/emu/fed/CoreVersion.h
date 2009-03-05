f01e8f588465638a2d27222afb79e50c8dfb1a6a
paste <paste>
2009-03-05 21:57:55 +0000

    * CAEN Bus Errors no longer throw exceptions. This is a temporary fix to a problem where the DCC does not send a DTACK on certain register writes, causing the controller to time out and set a bus error.  The write does not fail, so this is not actually an exception.

--- emuDCS/FEDCore/include/emu/fed/CoreVersion.h
+++ emuDCS/FEDCore/include/emu/fed/CoreVersion.h
@@ -1,7 +1,13 @@
 /*****************************************************************************\
-* $Id: CoreVersion.h,v 1.1 2009/03/05 16:02:14 paste Exp $
+* $Id: CoreVersion.h,v 1.2 2009/03/05 21:57:55 paste Exp $
 *
 * $Log: CoreVersion.h,v $
+* Revision 1.2  2009/03/05 21:57:55  paste
+* * CAEN Bus Errors no longer throw exceptions.
+* This is a temporary fix to a problem where the DCC does not send a DTACK on certain register writes,
+* causing the controller to time out and set a bus error.  The write does not fail, so this is not
+* actually an exception.
+*
 * Revision 1.1  2009/03/05 16:02:14  paste
 * * Shuffled FEDCrate libraries to new locations
 * * Updated libraries for XDAQ7
@@ -17,7 +23,7 @@
 
 namespace emufedcore {
 	const std::string package     = "emufedcore";
-	const std::string versions    = "07.00.00";
+	const std::string versions    = "07.00.01";
 	const std::string description = "Emu (CSC) FED crate core libraries";
 
 	const std::string summary     = "A collection of libraries for controlling end-cap muon CSC FED VME hardware.";
