c643630e04b585a90e7b81c07f2e96d21a288cb5
brett <brett>
2009-03-10 14:21:24 +0000

    added basic display and editing of configuration

--- emuDCS/PeripheralApps/include/EmuPCrateConfigTStore.h
+++ emuDCS/PeripheralApps/include/EmuPCrateConfigTStore.h
@@ -56,6 +56,11 @@ public:
   void outputException(xgi::Output * out,xcept::Exception &e);
   void outputStandardInterface(xgi::Output * out);
   
+  void displayConfiguration(xgi::Output * out,const std::string &configName,unsigned int index,const std::string &identifier);
+  void displayConfiguration(xgi::Output * out,const std::string &configName,unsigned int index,int crateID);
+  void outputTableEditControls(xgi::Output * out,xdata::Table &results,const std::string &tableName);
+  void outputTable(xgi::Output * out,xdata::Table &results);
+  
   // Actions
   void parseConfigFromXML(xgi::Input * in, xgi::Output * out ) throw (xgi::exception::Exception);
   void uploadConfigToDB(xgi::Input * in, xgi::Output * out ) throw (xgi::exception::Exception);
@@ -63,7 +68,8 @@ public:
   void synchronizeToFromDB(xgi::Input * in, xgi::Output * out ) throw (xgi::exception::Exception);
   void SelectConfFile(xgi::Input * in, xgi::Output * out ) throw (xgi::exception::Exception);
   void SetTypeDesc(xgi::Input * in, xgi::Output * out ) throw (xgi::exception::Exception);
-
+  void incrementValue(xgi::Input * in, xgi::Output * out ) throw (xgi::exception::Exception);
+  void setValue(xgi::Input * in, xgi::Output * out ) throw (xgi::exception::Exception);
   // Communication with TStore
   xoap::MessageReference sendSOAPMessage(xoap::MessageReference &message) throw (xcept::Exception);
   std::string connect() throw (xcept::Exception);
@@ -84,15 +90,15 @@ public:
   xdata::UnsignedInteger64 getConfigId(const std::string &dbTable, const std::string &dbColumn, const std::string endcap_side) throw (xcept::Exception);
   void uploadConfiguration(const std::string &connectionID, const std::string endcap_side) throw (xcept::Exception);
   void uploadPeripheralCrate(const std::string &connectionID, const std::vector<Crate *> &TStore_allCrates) throw (xcept::Exception);
-  void uploadCCB(const std::string &connectionID, xdata::UnsignedInteger64 &periph_config_id, CCB * TStore_thisCCB) throw (xcept::Exception);
-  void uploadMPC(const std::string &connectionID, xdata::UnsignedInteger64 &periph_config_id, MPC * TStore_thisMPC) throw (xcept::Exception);
-  void uploadVMECC(const std::string &connectionID, xdata::UnsignedInteger64 &periph_config_id, Crate * TStore_thisCrate) throw (xcept::Exception);
-  void uploadCSC(const std::string &connectionID, xdata::UnsignedInteger64 &periph_config_id, const std::vector<Chamber *> &TStore_allChambers) throw (xcept::Exception);
-  void uploadDAQMB(const std::string &connectionID, xdata::UnsignedInteger64 &csc_config_id, DAQMB * &TStore_thisDAQMB) throw (xcept::Exception);
-  void uploadTMB(const std::string &connectionID, xdata::UnsignedInteger64 &csc_config_id, TMB * &TStore_thisTMB) throw (xcept::Exception);
-  void uploadALCT(const std::string &connectionID, xdata::UnsignedInteger64 &tmb_config_id, ALCTController * &TStore_thisALCT) throw (xcept::Exception);
-  void uploadAnodeChannel(const std::string &connectionID, xdata::UnsignedInteger64 &alct_config_id, ALCTController * &TStore_thisALCT) throw (xcept::Exception);
-  void uploadCFEB(const std::string &connectionID, xdata::UnsignedInteger64 &daqmb_config_id, DAQMB * &TStore_thisDAQMB) throw (xcept::Exception);
+  void uploadCCB(const std::string &connectionID, xdata::UnsignedInteger64 &periph_config_id, int crateID) throw (xcept::Exception);
+  void uploadMPC(const std::string &connectionID, xdata::UnsignedInteger64 &periph_config_id, int crateID) throw (xcept::Exception);
+  void uploadVMECC(const std::string &connectionID, xdata::UnsignedInteger64 &periph_config_id, int crateID) throw (xcept::Exception);
+  void uploadCSC(const std::string &connectionID, xdata::UnsignedInteger64 &periph_config_id, const std::vector<Chamber *> &TStore_allChambers,int crateID) throw (xcept::Exception);
+  void uploadDAQMB(const std::string &connectionID, xdata::UnsignedInteger64 &csc_config_id, DAQMB * &TStore_thisDAQMB,int slot,const std::string &chamber) throw (xcept::Exception);
+  void uploadTMB(const std::string &connectionID, xdata::UnsignedInteger64 &csc_config_id, TMB * &TStore_thisTMB,int slot,const std::string &chamber) throw (xcept::Exception);
+  void uploadALCT(const std::string &connectionID, xdata::UnsignedInteger64 &tmb_config_id, ALCTController * &TStore_thisALCT,const std::string &identifier) throw (xcept::Exception);
+  void uploadAnodeChannel(const std::string &connectionID, xdata::UnsignedInteger64 &alct_config_id, ALCTController * &TStore_thisALCT,const std::string &identifier) throw (xcept::Exception);
+  void uploadCFEB(const std::string &connectionID, xdata::UnsignedInteger64 &daqmb_config_id,const std::string &identifier) throw (xcept::Exception);
 
   // DB Data Reads
   EmuEndcap * getConfiguredEndcap(const std::string &emu_config_id) throw (xcept::Exception);
@@ -103,13 +109,35 @@ public:
   void readMPC(const std::string &connectionID, const std::string &emu_config_id, const std::string &periph_config_id, Crate * theCrate) throw (xcept::Exception);
   void readCSC(const std::string &connectionID, const std::string &emu_config_id, const std::string &periph_config_id, Crate * theCrate) throw (xcept::Exception);
   void readDAQMB(const std::string &connectionID, const std::string &emu_config_id, const std::string &csc_config_id, Crate * theCrate, Chamber * theChamber) throw (xcept::Exception);
-  void readCFEB(const std::string &connectionID, const std::string &emu_config_id, const std::string &daqmb_config_id, DAQMB * theDaqmb) throw (xcept::Exception);
+  void readCFEB(const std::string &connectionID, const std::string &emu_config_id, const std::string &daqmb_config_id, DAQMB * theDaqmb,const std::string &cacheIdentifier) throw (xcept::Exception);
   void readTMB(const std::string &connectionID, const std::string &emu_config_id, const std::string &csc_config_id, Crate * theCrate, Chamber * theChamber) throw (xcept::Exception);
-  void readALCT(const std::string &connectionID, const std::string &emu_config_id, const std::string &tmb_config_id, TMB * theTmb) throw (xcept::Exception);
-  void readAnodeChannel(const std::string &connectionID, const std::string &emu_config_id, const std::string &alct_config_id, ALCTController * theAlct) throw (xcept::Exception);
+  void readALCT(const std::string &connectionID, const std::string &emu_config_id, const std::string &tmb_config_id, TMB * theTmb,const std::string &identifier) throw (xcept::Exception);
+  void readAnodeChannel(const std::string &connectionID, const std::string &emu_config_id, const std::string &alct_config_id, ALCTController * theAlct,const std::string &identifier) throw (xcept::Exception);
 
 private:
+	void setConfigID(xdata::Table &newRows,size_t rowId,const std::string &columnName,xdata::UnsignedInteger64 &id);
+	std::string crateIdentifierString(int crateID);
+	std::string chamberID(int crateID,const std::string &chamberLabel);
+	std::string DAQMBID(const std::string &chamber,int slot);
+	std::string getEndcapSide(std::vector<Crate *> &myCrates) throw (xcept::Exception);
+	xdata::Table &getCachedTable(const std::string &insertViewName,const std::string &identifier) throw (xcept::Exception);
+	void setCachedTable(const std::string &insertViewName,const std::string &identifier,xdata::Table &table) throw (xcept::Exception);
 
+	xdata::Table &getCachedTable(const std::string &insertViewName,int crateIndex) throw (xcept::Exception);
+	void setCachedTable(const std::string &insertViewName,int crateIndex,xdata::Table &table) throw (xcept::Exception);
+	bool canChangeColumn(const std::string &columnName);
+	bool isNumericType(const std::string &xdataType);
+	void getTableDefinitionsIfNecessary() throw ();
+	void getTableDefinitions(const std::string &connectionID);
+	void copyAnodeChannelToTable(xdata::Table &newRows,ALCTController * &TStore_thisALCT);
+	void copyALCTToTable(xdata::Table &newRows,ALCTController * &TStore_thisALCT);
+	void copyTMBToTable(xdata::Table &newRows,TMB * TStore_thisTMB);
+	void copyCFEBToTable(xdata::Table &newRows,DAQMB * TStore_thisDAQMB);
+	void copyDAQMBToTable(xdata::Table &newRows,DAQMB * TStore_thisDAQMB);
+	void copyCSCToTable(xdata::Table &newRows,Chamber * chamber);
+	void copyMPCToTable(xdata::Table &newRows,Crate * TStore_thisCrate);
+	void copyCCBToTable(xdata::Table &newRows,Crate * TStore_thisCrate);
+	void copyVMECCToTable(xdata::Table &newRows,Crate * TStore_thisCrate);
   std::string config_type_;
   std::string config_desc_;
   std::string xmlpath_;
@@ -130,6 +158,16 @@ private:
   xdata::Table tableDefinition_emu_tmb;
   xdata::Table tableDefinition_emu_alct;
   xdata::Table tableDefinition_emu_anodechannel;
+  
+  //the data is kept as tables in memory, to allow a common interface for changing values
+  //the most recent data is always in these tables.
+  //the key in the first map is the table name as passed to TStore (e.g. "vcc", "tmb")
+  //the value is a map of all tables which come from a query of that table (e.g. vcc from a particular crate)
+  //the key in this inside map can be anything which uniquely identifies that particular table 
+  //so it can be edited.
+  //it is named hierarchically beginning with the crate identifier so that it's easy to loop through
+  //just the tables relating to a particular crate or chamber to change the values.
+  std::map<std::string,std::map<std::string,xdata::Table> > currentTables;
 
 };
 
@@ -155,7 +193,7 @@ inline bool convertToHex(std::string &dataHex, const std::string format, const T
   if (err==0){
     std::cout << "Could not convert " << to_string(data) << " to hexadecimal format" << std::endl;
     dataHex = "";
-  }else{
+  } else{
     dataHex = to_string(buffer);
     converted = true;
   }
