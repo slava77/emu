46348f680ebd022b71b7520ffaed9667aa95f2f2
liu <liu>
2009-03-15 13:24:48 +0000

    version 7.0

--- emuDCS/OnlineDB/src/common/OnlineDBVersion.cc
+++ emuDCS/OnlineDB/src/common/OnlineDBVersion.cc
@@ -2,7 +2,7 @@
 
 namespace EmuOnlineDB {
         const std::string package  =  "EmuOnlineDB";
-        const std::string versions =  "6.4";
+        const std::string versions =  "7.00";
         const std::string summary = "emu/emuDCS/OnlineDB";
         const std::string description = "Emu Online Database Library";
         const std::string authors = "Stan Durkin";
