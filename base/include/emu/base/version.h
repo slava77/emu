ebd0cdba8e335f5b6b443ae8d17d7209a57aada9
liu <liu>
2009-05-26 10:54:24 +0000

    version 7.4

--- base/include/emu/base/version.h
+++ base/include/emu/base/version.h
@@ -5,7 +5,7 @@
 
 namespace emubase {
 	const std::string package     = "emubase";
-	const std::string versions    = "07.03.00";
+	const std::string versions    = "07.04.00";
 	const std::string description = "Emu (CSC) applications' base classes";
 
 	const std::string summary     = "emu/base";
--- supervisor/include/emu/supervisor/version.h
+++ supervisor/include/emu/supervisor/version.h
@@ -5,7 +5,7 @@
 
 namespace emusupervisor {
 	const std::string package     = "emusupervisor";
-	const std::string versions    = "07.03.00";
+	const std::string versions    = "07.04.00";
 	const std::string description = "Emu (CSC) Supervisor";
 
 	const std::string summary     = "emu/supervisor";
