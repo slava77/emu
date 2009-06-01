cef87d892f1f6407c7cc123037aee34419614a80
banicz <banicz>
2009-06-01 17:16:04 +0000

    version 07.05.00

--- daq/ta/include/emu/daq/ta/version.h
+++ daq/ta/include/emu/daq/ta/version.h
@@ -5,7 +5,7 @@
 
 namespace emudaqta {
 	const std::string package     = "emudaqta";
-	const std::string versions    = "09.01.30";
+	const std::string versions    = "07.05.00";
 	const std::string description = "Emu (CSC) Trigger Adapter";
 
 	const std::string summary     = "emu/daq/ta";
