7117cd9328d5e7dc0682098bafe41a1d6dee4b15
paste <paste>
2009-06-03 09:05:11 +0000

    Added hack to allow TF interrupt threads without actually using interrupts.

--- emuDCS/FEDUtils/include/emu/fed/UtilsVersion.h
+++ emuDCS/FEDUtils/include/emu/fed/UtilsVersion.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
-* $Id: UtilsVersion.h,v 1.8 2009/05/29 11:25:09 paste Exp $
+* $Id: UtilsVersion.h,v 1.9 2009/06/03 09:05:11 paste Exp $
 \*****************************************************************************/
 #ifndef __EMU_FED_UTILSVERSION_H__
 #define __EMU_FED_UTILSVERSION_H__
@@ -8,7 +8,7 @@
 
 namespace emuFEDUtils {
 	const std::string package     = "FEDUtils";
-	const std::string versions    = "07.05.00";
+	const std::string versions    = "07.05.01";
 	const std::string description = "Emu (CSC) FED Utility Libraries";
 
 	const std::string summary     = "Utility libraries used by the Front End Device (FED) crate XDAQ applications (FEDApps) for the CMS Emu (CSC) subdetector";
