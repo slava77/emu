416986b4cf66f626abf7f24c01bb00476c6aa89d
banicz <banicz@4525493e-7705-40b1-a816-d608a930855b>
2009-07-06 20:37:40 +0000

    Changed version to 07.06.00
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@5518 4525493e-7705-40b1-a816-d608a930855b

--- farmer/include/emu/farmer/version.h
+++ farmer/include/emu/farmer/version.h
@@ -5,7 +5,7 @@
 
 namespace emufarmer {
 	const std::string package     = "emufarmer";
-	const std::string versions    = "07.05.00";
+	const std::string versions    = "07.06.00";
 	const std::string description = "Emu (CSC) Farmer";
 
 	const std::string summary     = "emu/farmer";
