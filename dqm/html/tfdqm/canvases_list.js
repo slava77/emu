3c1ac5b895ed1e8b870e56444910113896c87d58
barvic <barvic@4525493e-7705-40b1-a816-d608a930855b>
2009-07-20 21:35:03 +0000

    Added DQM browser Web interface files
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@5553 4525493e-7705-40b1-a816-d608a930855b

---
+++ dqm/html/tfdqm/canvases_list.js
@@ -0,0 +1,34 @@
+var TREE_ITEMS = [
+	['EMU', ' ',
+           ['DDU FMM/TTS','DDU_status.png'],
+	   ['Data Integrity Timeline','integrity.png'],
+	   ['Occupancy','occupancy.png']
+	],
+	['TF', ' ',
+	   ['TF Rate','rate.png'],
+	   ['TF Timing','timing.png'],
+ 	   ['TF Tracks','tracks.png'],
+	   ['TF Counters','Counters.png'],
+	   ['Event Record Header', 'EventRecordHeader.png'],
+	   ['Data Block Header 1', 'DataBlockHeader1.png'],
+	   ['Data Block Header 2', 'DataBlockHeader2.png'],
+	   ['DBH1 Timeline', 'DBH1timeline.png'],
+	   ['DBH2 Timeline', 'DBH2timeline.png'],
+	   ['DT Data 1', 'dt_data1.png'],
+	   ['DT Data 2', 'dt_data2.png']
+	],
+	['Inputs', ' ',
+	   ['CSCs Occupancy', 'csc.png'],
+	   ['Links Status', 'links.png']
+	],
+	['Chambers', ' ',
+	   ['Relative Timing','csc_timing.png'], 
+	   ['CLCT Pattern','CLCT.png'],
+	   ['LCT Quality','LCTquality.png'],
+	   ['LCT TimeBins','time_bin.png'],
+	   ['Quality Consistency','qualityConsistency.png'],
+	   ['Status','status.png'],
+	   ['Strips Occupancy','Strips.png'],
+	   ['WireGroups Occupancy','WireGroup.png']	   
+        ]
+];
