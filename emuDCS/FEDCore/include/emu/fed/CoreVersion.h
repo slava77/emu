a44fa3eee045f829c1c21a70a20a05ec43ece7fa
liu <liu>
2009-08-16 16:09:12 +0000

    version 7.8

--- emuDCS/FEDCore/include/emu/fed/CoreVersion.h
+++ emuDCS/FEDCore/include/emu/fed/CoreVersion.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
-* $Id: CoreVersion.h,v 1.22 2009/08/15 19:16:27 paste Exp $
+* $Id: CoreVersion.h,v 1.23 2009/08/16 16:09:11 liu Exp $
 \*****************************************************************************/
 
 #ifndef __EMU_FED_COREVERSION_H__
@@ -9,7 +9,7 @@
 
 namespace EmuFEDCore {
 	const std::string package     = "EmuFEDCore";
-	const std::string versions    = "07.09.00";
+	const std::string versions    = "07.08.00";
 	const std::string description = "Emu (CSC) FED Core Libraries";
 
 	const std::string summary     = "Core libraries for controlling Front End Device (FED) crates for the CMS Emu (CSC) subdetector";
