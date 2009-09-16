cf94c8b1366579d4fe8778aab1396e47eac53f60
banicz <banicz@4525493e-7705-40b1-a816-d608a930855b>
2009-09-16 18:35:58 +0000

    For use in bdg 904.
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@5678 4525493e-7705-40b1-a816-d608a930855b

---
+++ farmer/xml/generateConfigsBdg904.sh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+
+
+# Create a dir for the output if it doesn't exist yet
+OUTDIR=out
+[ -d $OUTDIR ] || mkdir $OUTDIR
+
+# .duck for DQM_Display:
+echo "xsltproc --stringparam FARM DQM_Display --stringparam NAME DAQ_writeN_buildN EmuDAQ904DUCKGenerator.xsl RUI-to-computer_mapping_bdg904.xml > $OUTDIR/DQM_Display.duck"
+xsltproc --stringparam FARM DQM_Display --stringparam NAME DAQ_writeN_buildN EmuDAQ904DUCKGenerator.xsl RUI-to-computer_mapping_bdg904.xml > $OUTDIR/DQM_Display.duck
+
+
+# .duck for DQM:
+echo "xsltproc --stringparam FARM DQM --stringparam NAME DAQ_writeN_buildN EmuDAQ904DUCKGenerator.xsl RUI-to-computer_mapping_bdg904.xml > $OUTDIR/DQM.duck"
+xsltproc --stringparam FARM DQM --stringparam NAME DAQ_writeN_buildN EmuDAQ904DUCKGenerator.xsl RUI-to-computer_mapping_bdg904.xml > $OUTDIR/DQM.duck
+# loop over all parameter combinations for DAQ:
+for WRITE in Y N; do
+    for BUILD in N; do
+	NAME=DAQ_write${WRITE}_build${BUILD}
+	# .duck for DAQ:
+	echo "xsltproc --stringparam FARM DAQ --stringparam NAME $NAME EmuDAQ904DUCKGenerator.xsl RUI-to-computer_mapping_bdg904.xml > $OUTDIR/$NAME.duck"
+	xsltproc --stringparam FARM DAQ --stringparam NAME $NAME EmuDAQ904DUCKGenerator.xsl RUI-to-computer_mapping_bdg904.xml > $OUTDIR/$NAME.duck
+        # common .xml for DAQ and DQM and DQM_Display:
+	echo "xsltproc --stringparam WRITE $WRITE --stringparam BUILD $BUILD EmuDAQ904ConfigGenerator.xsl RUI-to-computer_mapping_bdg904.xml > $OUTDIR/$NAME.xml"
+	xsltproc --stringparam WRITE $WRITE --stringparam BUILD $BUILD EmuDAQ904ConfigGenerator.xsl RUI-to-computer_mapping_bdg904.xml > $OUTDIR/$NAME.xml
+    done
+done
