a74bbde9667a000efdb096e3c66ba584aaeaf90c
liu <liu@4525493e-7705-40b1-a816-d608a930855b>
2009-11-09 21:21:01 +0000

    version 7.13
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@5795 4525493e-7705-40b1-a816-d608a930855b

--- emuDCS/FEDApps/include/emu/fed/AppsVersion.h
+++ emuDCS/FEDApps/include/emu/fed/AppsVersion.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
-* $Id: AppsVersion.h,v 1.45 2009/11/09 11:43:52 paste Exp $
+* $Id: AppsVersion.h,v 1.46 2009/11/09 21:19:21 liu Exp $
 \*****************************************************************************/
 
 #ifndef __EMU_FED_APPSVERSION_H__
@@ -9,7 +9,7 @@
 
 namespace EmuFEDApps {
 	const std::string package     = "EmuFEDApps";
-	const std::string versions    = "09.00.03";
+	const std::string versions    = "07.13.00";
 	const std::string description = "Emu (CSC) FED XDAQ Applications";
 
 	const std::string summary     = "Applications to control and monitor the Front End Device (FED) crates for the CMS Emu (CSC) subdetector";
