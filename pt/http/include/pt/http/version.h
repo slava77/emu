23712e5cd9166f0f3a7ff8cfb048e28834d4dd20
banicz <banicz>
2010-02-23 13:21:55 +0000

    PTHTTP --> EMUPTHTTP

--- pt/http/include/pt/http/version.h
+++ pt/http/include/pt/http/version.h
@@ -1,4 +1,4 @@
-// $Id: version.h,v 1.4 2010/02/11 22:02:16 banicz Exp $
+// $Id: version.h,v 1.5 2010/02/23 13:21:55 banicz Exp $
 
 /*************************************************************************
  * XDAQ Components for Distributed Data Acquisition                      *
@@ -25,26 +25,26 @@
 // // If any previous versions available E.g. #define PTHTTP_PREVIOUS_VERSIONS "3.8.0,3.8.1"
 // #define PTHTTP_PREVIOUS_VERSIONS "3.10.0,3.10.1,3.10.2,3.10.3,3.10.4,3.10.5,3.10.6,3.10.7,3.10.8,3.10.9,3.10.10,3.10.11,3.10.12"
 
-#define PTHTTP_VERSION_MAJOR 1
-#define PTHTTP_VERSION_MINOR 0
-#define PTHTTP_VERSION_PATCH 0
-// If any previous versions available E.g. #define PTHTTP_PREVIOUS_VERSIONS "3.8.0,3.8.1"
-#define PTHTTP_PREVIOUS_VERSIONS ""
+#define EMUPTHTTP_VERSION_MAJOR 1
+#define EMUPTHTTP_VERSION_MINOR 0
+#define EMUPTHTTP_VERSION_PATCH 0
+// If any previous versions available E.g. #define EMUPTHTTP_PREVIOUS_VERSIONS "3.8.0,3.8.1"
+#define EMUPTHTTP_PREVIOUS_VERSIONS ""
 
 
 //
 // Template macros
 //
-#define PTHTTP_VERSION_CODE PACKAGE_VERSION_CODE(PTHTTP_VERSION_MAJOR,PTHTTP_VERSION_MINOR,PTHTTP_VERSION_PATCH)
-#ifndef PTHTTP_PREVIOUS_VERSIONS
-#define PTHTTP_FULL_VERSION_LIST  PACKAGE_VERSION_STRING(PTHTTP_VERSION_MAJOR,PTHTTP_VERSION_MINOR,PTHTTP_VERSION_PATCH)
+#define EMUPTHTTP_VERSION_CODE PACKAGE_VERSION_CODE(EMUPTHTTP_VERSION_MAJOR,EMUPTHTTP_VERSION_MINOR,EMUPTHTTP_VERSION_PATCH)
+#ifndef EMUPTHTTP_PREVIOUS_VERSIONS
+#define EMUPTHTTP_FULL_VERSION_LIST  PACKAGE_VERSION_STRING(EMUPTHTTP_VERSION_MAJOR,EMUPTHTTP_VERSION_MINOR,EMUPTHTTP_VERSION_PATCH)
 #else 
-#define PTHTTP_FULL_VERSION_LIST  PTHTTP_PREVIOUS_VERSIONS "," PACKAGE_VERSION_STRING(PTHTTP_VERSION_MAJOR,PTHTTP_VERSION_MINOR,PTHTTP_VERSION_PATCH)
+#define EMUPTHTTP_FULL_VERSION_LIST  EMUPTHTTP_PREVIOUS_VERSIONS "," PACKAGE_VERSION_STRING(EMUPTHTTP_VERSION_MAJOR,EMUPTHTTP_VERSION_MINOR,EMUPTHTTP_VERSION_PATCH)
 #endif 
 namespace emupthttp
 {
     const std::string package  =  "emupthttp";
-    const std::string versions =  PTHTTP_FULL_VERSION_LIST;
+    const std::string versions =  EMUPTHTTP_FULL_VERSION_LIST;
     const std::string summary = "HTTP peer transport with SOAP and CGI service implementations modified to comply with SOAP 1.1";
     const std::string description = "Based on daq/pt/http v3.10.13";
     const std::string authors = "Johannes Gutleber, Luciano Orsini, Karoly Banicz";
