87a8be35de41e3985258613dff75ac479cef2033
banicz <banicz>
2010-03-25 20:28:00 +0000

    Added number of events written to files.

--- daq/rui/include/emu/daq/rui/FactTypes.h
+++ daq/rui/include/emu/daq/rui/FactTypes.h
@@ -5,15 +5,15 @@ namespace emu { namespace daq { namespace rui {
 
   class RUIStatusFact {
   public:
-    enum ParameterName_t { runType, state, nEventsRead, isWritingToFile, nParameters };
+    enum ParameterName_t { runType, state, nEventsRead, isWritingToFile, nEventsWritten, nParameters };
     static const char*  getTypeName() { return typeName_; }
     static const string getParameterName( const ParameterName_t p ) { return parameterNames_[p]; }
   protected:
     static const char* const typeName_;
     static const char* const parameterNames_[nParameters];
   };
-  const char* const RUIStatusFact::typeName_ = "RUIStatusFact";
-  const char* const RUIStatusFact::parameterNames_[] = { "runType", "state", "nEventsRead", "isWritingToFile" };
+  const char* const RUIStatusFact::typeName_ = "ruiStatusFact";
+  const char* const RUIStatusFact::parameterNames_[] = { "runType", "state", "nEventsRead", "isWritingToFile", "nEventsWritten" };
 
 }}} // namespace emu::daq::rui
 
