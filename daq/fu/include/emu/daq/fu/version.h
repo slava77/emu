bd6f3351124585114cfff5dba99b6b07cbdf2c7a
banicz <banicz@4525493e-7705-40b1-a816-d608a930855b>
2010-05-28 21:04:15 +0000

    Version 09.09.00
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@6107 4525493e-7705-40b1-a816-d608a930855b

--- daq/fu/include/emu/daq/fu/version.h
+++ daq/fu/include/emu/daq/fu/version.h
@@ -5,7 +5,7 @@
 
 namespace emudaqfu {
 	const std::string package     = "emudaqfu";
-	const std::string versions    = "09.00.00";
+	const std::string versions    = "09.09.00";
 	const std::string description = "Emu (CSC) Filter Unit";
 
 	const std::string summary     = "emu/daq/fu";
--- daq/manager/include/emu/daq/manager/version.h
+++ daq/manager/include/emu/daq/manager/version.h
@@ -5,7 +5,7 @@
 
 namespace emudaqmanager {
 	const std::string package     = "emudaqmanager";
-	const std::string versions    = "09.00.00";
+	const std::string versions    = "09.09.00";
 	const std::string description = "Emu (CSC) Local DAQ Manager";
 
 	const std::string summary     = "emu/daq/manager";
