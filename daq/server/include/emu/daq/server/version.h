ca31458fc2293ff52e82e9cd83d651937ab1620b
banicz <banicz>
2010-05-28 21:04:15 +0000

    Version 09.09.00

--- daq/server/include/emu/daq/server/version.h
+++ daq/server/include/emu/daq/server/version.h
@@ -5,7 +5,7 @@
 
 namespace emudaqserver {
 	const std::string package     = "emudaqserver";
-	const std::string versions    = "09.00.00";
+	const std::string versions    = "09.09.00";
 	const std::string description = "Emu (CSC) DAQ servers";
 
 	const std::string summary     = "emu/daq/server";
