e32f65d05849a6155648c2bfb3cc42a010370279
banicz <banicz>
2010-06-01 12:44:43 +0000

    Version 09.09.01

--- supervisor/include/emu/supervisor/version.h
+++ supervisor/include/emu/supervisor/version.h
@@ -5,7 +5,7 @@
 
 namespace emusupervisor {
 	const std::string package     = "emusupervisor";
-	const std::string versions    = "09.09.00";
+	const std::string versions    = "09.09.01";
 	const std::string description = "Emu (CSC) Supervisor";
 
 	const std::string summary     = "emu/supervisor";
