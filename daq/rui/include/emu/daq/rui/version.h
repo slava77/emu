494a7a361c2cbcb4a2d010d1abe01834b3898f71
banicz <banicz>
2010-12-08 00:00:33 +0000

    Version 09.12.02

--- daq/rui/include/emu/daq/rui/version.h
+++ daq/rui/include/emu/daq/rui/version.h
@@ -5,7 +5,7 @@
 
 namespace emudaqrui {
 	const std::string package     = "emudaqrui";
-	const std::string versions    = "09.12.01";
+	const std::string versions    = "09.12.02";
 	const std::string description = "Emu (CSC) Readout Unit Input";
 
 	const std::string summary     = "emu/daq/rui";
