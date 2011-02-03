b4f8af0d494f4a6c64fbef0cd7821b2fde2ae2f9
banicz <banicz>
2011-02-03 18:27:53 +0000

    Version 09.13.00

--- daq/manager/include/emu/daq/manager/version.h
+++ daq/manager/include/emu/daq/manager/version.h
@@ -5,7 +5,7 @@
 
 namespace emudaqmanager {
 	const std::string package     = "emudaqmanager";
-	const std::string versions    = "09.12.02";
+	const std::string versions    = "09.13.00";
 	const std::string description = "Emu (CSC) Local DAQ Manager";
 
 	const std::string summary     = "emu/daq/manager";
