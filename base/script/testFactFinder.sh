406ae85d49cad4f58e78f8e590d04275dcc6a15e
banicz <banicz>
2011-03-24 23:42:05 +0000

    Lower case fact types.

--- base/script/testFactFinder.sh
+++ base/script/testFactFinder.sh
@@ -1,19 +1,18 @@
-sendSOAP -v  -u emuslice02 -p 20210 -A emu::daq::manager::Application -I 0 -b "
+sendSOAP -v  -u emuslice06 -p 20200 -A emu::daq::manager::Application -I 0 -b "
 <esd:factRequestCollection xmlns:esd='http://www.cern.ch/cms/csc/dw/model'>
     <esd:requestId>98765</esd:requestId>
     <esd:factRequest>
         <esd:component_id>emu::daq::rui::Application00</esd:component_id>
         <esd:component_id>RUI00</esd:component_id>
-        <esd:factType>ApplicationStatusFact</esd:factType>
+        <esd:factType>applicationStatusFact</esd:factType>
     </esd:factRequest>
     <esd:factRequest>
-        <esd:component_id>emu::daq::rui::Application01</esd:component_id>
         <esd:component_id>RUI01</esd:component_id>
-        <esd:factType>ApplicationStatusFact</esd:factType>
+        <esd:factType>applicationStatusFact</esd:factType>
     </esd:factRequest>
     <esd:factRequest>
         <esd:component_id>emu::daq::manager::Application</esd:component_id>
         <esd:component_id>DAQManager</esd:component_id>
-        <esd:factType>LocalDAQStatusFact</esd:factType>
+        <esd:factType>localDAQStatusFact</esd:factType>
     </esd:factRequest>
 </esd:factRequestCollection>"
