e96793280b02d676241a6d26cd4d0d75e6971084
banicz <banicz@4525493e-7705-40b1-a816-d608a930855b>
2011-03-27 18:40:20 +0000

    Version 10.01.00
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@6439 4525493e-7705-40b1-a816-d608a930855b

--- emuDCS/FEDApps/include/emu/fed/AppsVersion.h
+++ emuDCS/FEDApps/include/emu/fed/AppsVersion.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
-* $Id: AppsVersion.h,v 1.61 2011/02/10 09:24:48 banicz Exp $
+* $Id: AppsVersion.h,v 1.62 2011/03/27 18:40:19 banicz Exp $
 \*****************************************************************************/
 
 #ifndef __EMU_FED_APPSVERSION_H__
@@ -9,7 +9,7 @@
 
 namespace EmuFEDApps {
 	const std::string package     = "EmuFEDApps";
-	const std::string versions    = "10.00.00";
+	const std::string versions    = "10.01.00";
 	const std::string description = "Emu (CSC) FED XDAQ Applications";
 
 	const std::string summary     = "Applications to control and monitor the Front End Device (FED) crates for the CMS Emu (CSC) subdetector";
--- farmer/include/emu/farmer/version.h
+++ farmer/include/emu/farmer/version.h
@@ -5,7 +5,7 @@
 
 namespace emufarmer {
 	const std::string package     = "emufarmer";
-	const std::string versions    = "10.00.02";
+	const std::string versions    = "10.01.00";
 	const std::string description = "Emu (CSC) Farmer";
 
 	const std::string summary     = "emu/farmer";
