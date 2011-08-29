fa351b33d238c06444da2eecdbb42356087b7647
liu <liu@4525493e-7705-40b1-a816-d608a930855b>
2011-08-29 01:10:14 +0000

    version 10.04
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@6549 4525493e-7705-40b1-a816-d608a930855b

--- emuDCS/FEDCore/include/emu/fed/CoreVersion.h
+++ emuDCS/FEDCore/include/emu/fed/CoreVersion.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
-* $Id: CoreVersion.h,v 1.48 2011/07/04 12:44:07 liu Exp $
+* $Id: CoreVersion.h,v 1.49 2011/08/29 01:10:13 liu Exp $
 \*****************************************************************************/
 
 #ifndef __EMU_FED_COREVERSION_H__
@@ -9,7 +9,7 @@
 
 namespace EmuFEDCore {
 	const std::string package     = "EmuFEDCore";
-	const std::string versions    = "10.03.00";
+	const std::string versions    = "10.04.00";
 	const std::string description = "Emu (CSC) FED Core Libraries";
 
 	const std::string summary     = "Core libraries for controlling Front End Device (FED) crates for the CMS Emu (CSC) subdetector";
