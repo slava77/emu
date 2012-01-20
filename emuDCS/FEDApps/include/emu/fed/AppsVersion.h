a1d10e1baa2266eadacb224dd05defd483e8021a
liu <liu>
2012-01-20 18:14:42 +0000

    version 10.06

--- emuDCS/FEDApps/include/emu/fed/AppsVersion.h
+++ emuDCS/FEDApps/include/emu/fed/AppsVersion.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
-* $Id: AppsVersion.h,v 1.68 2011/11/07 15:38:34 liu Exp $
+* $Id: AppsVersion.h,v 1.69 2012/01/20 18:14:19 liu Exp $
 \*****************************************************************************/
 
 #ifndef __EMU_FED_APPSVERSION_H__
@@ -9,7 +9,7 @@
 
 namespace EmuFEDApps {
 	const std::string package     = "EmuFEDApps";
-	const std::string versions    = "10.05.00";
+	const std::string versions    = "10.06.00";
 	const std::string description = "Emu (CSC) FED XDAQ Applications";
 
 	const std::string summary     = "Applications to control and monitor the Front End Device (FED) crates for the CMS Emu (CSC) subdetector";
--- supervisor/include/emu/supervisor/version.h
+++ supervisor/include/emu/supervisor/version.h
@@ -5,7 +5,7 @@
 
 namespace emusupervisor {
 	const std::string package     = "emusupervisor";
-	const std::string versions    = "10.05.00";
+	const std::string versions    = "10.06.00";
 	const std::string description = "Emu (CSC) Supervisor";
 
 	const std::string summary     = "emu/supervisor";
