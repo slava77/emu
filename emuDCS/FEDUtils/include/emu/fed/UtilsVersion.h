74064dfaea7097efbc6602a89787cb42bac3b52d
banicz <banicz>
2012-02-03 21:49:38 +0000

    Version 11.00.00

--- emuDCS/FEDUtils/include/emu/fed/UtilsVersion.h
+++ emuDCS/FEDUtils/include/emu/fed/UtilsVersion.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
-* $Id: UtilsVersion.h,v 1.49 2012/01/20 18:14:27 liu Exp $
+* $Id: UtilsVersion.h,v 1.50 2012/02/03 21:49:38 banicz Exp $
 \*****************************************************************************/
 #ifndef __EMU_FED_UTILSVERSION_H__
 #define __EMU_FED_UTILSVERSION_H__
@@ -8,7 +8,7 @@
 
 namespace EmuFEDUtils {
 	const std::string package     = "EmuFEDUtils";
-	const std::string versions    = "10.06.00";
+	const std::string versions    = "11.00.00";
 	const std::string description = "Emu (CSC) FED Utility Libraries";
 
 	const std::string summary     = "Utility libraries used by the Front End Device (FED) crate XDAQ applications (FEDApps) for the CMS Emu (CSC) subdetector";
