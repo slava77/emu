14d4ba68b19469d5101d414cae47a0b41dd49346
liu <liu>
2012-02-04 10:06:49 +0000

    version 11.00

--- emuDCS/PeripheralXtop/src/common/XtopVersion.cc
+++ emuDCS/PeripheralXtop/src/common/XtopVersion.cc
@@ -2,7 +2,7 @@
 
 namespace EmuPeripheralXtop {
 	const std::string package  =  "EmuPeripheralXtop";
-	const std::string versions =  "10.06.00";
+	const std::string versions =  "11.00.00";
 	const std::string summary = "emu/emuDCS/PeripheralXtop";
 	const std::string description = "Emu Peripheral Crate Xmas to PVSS Library";
 	const std::string authors = "Jinghua Liu for cms-emu-onlinesoftware";
