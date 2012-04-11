e2adfb0e7a8b7639aa2a2dc7290ceea956ec90cc
khotilov <khotilov@4525493e-7705-40b1-a816-d608a930855b>
2012-04-11 21:39:25 +0000

    toHTML method
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@6802 4525493e-7705-40b1-a816-d608a930855b

--- base/include/emu/exception/Exception.h
+++ base/include/emu/exception/Exception.h
@@ -42,7 +42,9 @@ public:
   : xcept::Exception(name, message, module, line, function, previous)
   {}
 
-  /** Outputs exception in a form that is suitable for javascript parsing **/
+  /**
+   * Outputs exception in a form that is suitable for javascript parsing
+   */
   std::string toJSON()
   {
     std::ostringstream out;
@@ -59,12 +61,42 @@ public:
         out << ",";
       }
       // lousy last comma
-      out.seekp((long int) out.tellp() - 1);
+      out.seekp((size_t) out.tellp() - 1);
       out << "}";
     }
     // lousy last comma
-    out.seekp((long int) out.tellp() - 1);
+    out.seekp((size_t) out.tellp() - 1);
     out << "]}";
+    return out.str();
+  }
+
+  /**
+   * Outputs exception in an HTML form
+   */
+  std::string toHTML()
+  {
+    std::ostringstream out;
+
+    out << "<table class=\"Xcept\">";
+    out << "<tr><th colspan=\"3\">Exception stack</th></tr>";
+    out << "<tr><th style=\"width: 1%;\">#</th><th style=\"text-align: left;\">message</th><th style=\"text-align: left;\">type</th><th style=\"text-align: left;\">raised at</th></tr>";
+
+    std::vector<xcept::ExceptionInformation> & history = getHistory();
+    std::vector<xcept::ExceptionInformation>::reverse_iterator i = history.rbegin();
+    int iLayer = history.size();
+    while ( i != history.rend() )
+    {
+      out << "<tr>";
+      out << "<td style=\"width: 1%;background-color: #dddddd;\">" << iLayer << "</td>";
+      out << "<td style=\"text-align: left;\">" << i->getProperty("identifier") << "</td>";
+      out << "<td style=\"background-color: #ff5533;\">" << i->getProperty("message") << "</td>";
+      out << "<td style=\"text-align: left;\">" << i->getProperty("function") << " (" << i->getProperty("module") << ":" << i->getProperty("line") << ")</td>";
+      out << "</tr>";
+      ++i;
+      --iLayer;
+    }
+    out << "</table>";
+
     return out.str();
   }
 };
