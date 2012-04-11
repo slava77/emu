a4964dac73eabb5ae84b5fc4822afb08688f269c
khotilov <khotilov>
2012-04-11 21:34:47 +0000

    moved from src/common/ directory

---
+++ base/src/common/utils/Cgi.cc
@@ -0,0 +1,95 @@
+// $Id: Cgi.cc,v 1.1 2012/04/11 21:34:47 khotilov Exp $
+
+#include "emu/utils/Cgi.h"
+
+#include "cgicc/HTMLClasses.h"
+#include "toolbox/regex.h"
+
+#include <exception>
+#include <sstream>
+#include <iostream>
+
+
+std::map<std::string, std::string>
+emu::utils::selectFromQueryString( std::vector<cgicc::FormEntry>& fev, const std::string namePattern )
+throw( xcept::Exception )
+{
+  std::map< std::string, std::string > nameValue;
+  try
+  {
+    std::vector< cgicc::FormEntry >::const_iterator fe;
+    for (fe = fev.begin(); fe != fev.end(); ++fe)
+    {
+      bool isMatched = false;
+      try
+      {
+        isMatched = toolbox::regx_match(fe->getName(), namePattern);
+      }
+      catch (xcept::Exception& e)
+      {
+        std::stringstream ss;
+        ss << "Regex match of pattern \"" << namePattern << "\" failed in \"" << fe->getName() << "\": ";
+        XCEPT_RETHROW( xcept::Exception, ss.str(), e);
+      }
+      catch (std::exception& e)
+      {
+        std::stringstream ss;
+        ss << "Regex match of pattern \"" << namePattern << "\" failed in \"" << fe->getName() << "\": " << e.what();
+        XCEPT_RAISE( xcept::Exception, ss.str());
+      }
+      catch (...)
+      {
+        std::stringstream ss;
+        ss << "Regex match of pattern \"" << namePattern << "\" failed in \"" << fe->getName() << "\": Unexpected exception";
+        XCEPT_RAISE( xcept::Exception, ss.str());
+      }
+
+      if (isMatched) nameValue[fe->getName()] = fe->getValue();
+    }
+  }
+  catch (xcept::Exception& e)
+  {
+    XCEPT_RETHROW( xcept::Exception, "Failed to select from HTTP query string: ", e);
+  }
+  catch (std::exception& e)
+  {
+    std::stringstream ss;
+    ss << "Failed to select from HTTP query string: " << e.what();
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+  catch (...)
+  {
+    XCEPT_RAISE( xcept::Exception, "Failed to select from HTTP query string: Unexpected exception");
+  }
+
+  return nameValue;
+}
+
+
+void emu::utils::redirect(xgi::Input *in, xgi::Output *out)
+{
+  // Redirect for the action not to be repeated if the 'reload' button is hit.
+  // cout << "PATH_TRANSLATED: " <<  in->getenv( "PATH_TRANSLATED" ) << endl
+  //      << "PATH_INFO: " << in->getenv( "PATH_INFO" ) << endl;
+  std::string url = in->getenv("PATH_TRANSLATED");
+  std::size_t p = url.find("/" + in->getenv("PATH_INFO") + "?");
+  if (p != std::string::npos)
+  {
+    cgicc::HTTPResponseHeader &header = out->getHTTPResponseHeader();
+    header.getStatusCode(303);
+    header.getReasonPhrase("See Other");
+    // Use the following line to redirect to the default web page in the same directory.
+    header.addHeader("Location", url.substr(0, p) + "/");
+    // Use the following line to redirect to the same page stripped of the query string.
+    // header.addHeader( "Location", url.substr(0, p) + "/" + in->getenv("PATH_INFO") );
+  }
+}
+
+
+void emu::utils::redirectTo(const std::string newURL, xgi::Output *out)
+{
+  cgicc::HTTPResponseHeader &header = out->getHTTPResponseHeader();
+  header.getStatusCode(303);
+  header.getReasonPhrase("See Other");
+  header.addHeader("Location", newURL);
+}
---
+++ base/src/common/utils/Xalan.cc
@@ -0,0 +1,373 @@
+// $Id: Xalan.cc,v 1.1 2012/04/11 21:34:47 khotilov Exp $
+
+#include "emu/utils/Xalan.h"
+
+#include <xercesc/util/PlatformUtils.hpp>
+#include <xercesc/framework/MemBufInputSource.hpp>
+#include <xercesc/util/XMLException.hpp>
+#include "xercesc/sax/SAXParseException.hpp"
+
+#include <xalanc/XalanTransformer/XalanTransformer.hpp>
+#include <xalanc/PlatformSupport/XSLException.hpp>
+#include <xalanc/PlatformSupport/XalanStdOutputStream.hpp>
+#include <xalanc/PlatformSupport/XalanOutputStreamPrintWriter.hpp>
+#include <xalanc/XMLSupport/FormatterToXML.hpp>
+#include <xalanc/XMLSupport/FormatterTreeWalker.hpp>
+#include <xalanc/XPath/XPathEvaluator.hpp>
+#include <xalanc/XalanSourceTree/XalanSourceTreeDOMSupport.hpp>
+#include <xalanc/DOMSupport/XalanDocumentPrefixResolver.hpp>
+#include "xalanc/XercesParserLiaison/XercesDOMSupport.hpp"
+#include "xalanc/XercesParserLiaison/XercesParserLiaison.hpp"
+
+#include "xoap/domutils.h" // for XMLCh2String
+
+#include <sstream>
+#include <exception>
+
+
+int emu::utils::transformStreams(std::istream& source, std::istream& stylesheet, std::ostream& target)
+throw( xcept::Exception )
+{
+  XALAN_USING_XALAN( XSLException );
+  int result = 1;
+  try
+  {
+    XALAN_USING_XALAN(XalanTransformer);    
+    // Initialize Xalan.
+    XalanTransformer::initialize();
+    // Create a XalanTransformer.
+    XalanTransformer theXalanTransformer;
+
+    XALAN_USING_XALAN(XSLTInputSource);
+    XALAN_USING_XALAN(XSLTResultTarget);
+    XSLTInputSource theSource( source );
+    XSLTInputSource theStylesheet( stylesheet );
+    XSLTResultTarget theTarget( target );
+
+    // Do the transform.
+    result = theXalanTransformer.transform(theSource, theStylesheet, theTarget);
+    
+    if (result != 0)
+    {
+      std::stringstream ss;
+      ss << "Error: " << theXalanTransformer.getLastError();
+      XCEPT_RAISE( xcept::Exception, ss.str());
+    }
+
+    // Terminate Xalan...
+    XalanTransformer::terminate();
+    // Clean up the ICU, if it's integrated...
+    XalanTransformer::ICUCleanUp();
+  }
+  catch (xcept::Exception& e)
+  {
+    XCEPT_RETHROW( xcept::Exception, "XSLT transformation failed: ", e);
+  }
+  catch (XSLException& e)
+  {
+    XALAN_USING_XALAN(XalanDOMString)
+    std::stringstream ss;
+    ss << "XSLT transformation failed: XSLException type: " << XalanDOMString(e.getType()) << ", message: "
+        << e.getMessage();
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+  catch (std::exception& e)
+  {
+    std::stringstream ss;
+    ss << "XSLT transformation failed: " << e.what();
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+  catch (...)
+  {
+    XCEPT_RAISE( xcept::Exception, "XSLT transformation failed: Unknown exception.");
+  }
+  return result;
+}
+
+
+int emu::utils::transformWithParams( std::istream& source, std::istream& stylesheet, std::ostream& target,
+                                     const std::map< std::string, std::string >& params)
+throw( xcept::Exception )
+{
+  XALAN_USING_XALAN( XSLException );
+  int result = 1;
+  try
+  {
+    XALAN_USING_XALAN(XalanTransformer);    
+    // Initialize Xalan.
+    XalanTransformer::initialize();
+    // Create a XalanTransformer.
+    XalanTransformer theXalanTransformer;
+
+    XALAN_USING_XALAN(XSLTInputSource);
+    XALAN_USING_XALAN(XSLTResultTarget);
+    XSLTInputSource theSource( source );
+    XSLTInputSource theStylesheet( stylesheet );
+    XSLTResultTarget theTarget( target );
+
+    // Set the stylesheet parameters
+    XALAN_USING_XALAN(XalanDOMString);
+    for (std::map< std::string, std::string >::const_iterator p = params.begin(); p != params.end(); ++p)
+    {
+      XalanDOMString name(p->first.c_str());
+      XalanDOMString value(p->second.c_str());
+      theXalanTransformer.setStylesheetParam(name, value);
+    }
+    // Do the transform.
+    result = theXalanTransformer.transform(theSource, theStylesheet, theTarget);
+    
+    if (result != 0)
+    {
+      std::stringstream ss;
+      ss << "Error: " << theXalanTransformer.getLastError();
+      XCEPT_RAISE( xcept::Exception, ss.str());
+    }
+
+    // Terminate Xalan...
+    XalanTransformer::terminate();
+    // Clean up the ICU, if it's integrated...
+    XalanTransformer::ICUCleanUp();
+  }
+  catch (xcept::Exception& e)
+  {
+    XCEPT_RETHROW( xcept::Exception, "XSLT transformation failed: ", e);
+  }
+  catch (XSLException& e)
+  {
+    XALAN_USING_XALAN(XalanDOMString)
+    std::stringstream ss;
+    ss << "XSLT transformation failed: XSLException type: " << XalanDOMString(e.getType()) << ", message: " << e.getMessage();
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+  catch (std::exception& e)
+  {
+    std::stringstream ss;
+    ss << "XSLT transformation failed: " << e.what();
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+  catch (...)
+  {
+    XCEPT_RAISE( xcept::Exception, "XSLT transformation failed: Unknown exception.");
+  }
+  return result;
+}
+
+
+XALAN_USING_XALAN(XalanDocument)
+
+
+std::string emu::utils::serialize( const XalanNode* node ) throw( xcept::Exception )
+{
+  XALAN_USING_XALAN(XalanStdOutputStream);
+  XALAN_USING_XALAN(XalanOutputStreamPrintWriter);
+  XALAN_USING_XALAN(FormatterToXML);
+  XALAN_USING_XALAN(FormatterTreeWalker);
+
+  std::stringstream target_stream; // this is the output stream
+  XalanStdOutputStream output_stream(target_stream);
+  XalanOutputStreamPrintWriter writer(output_stream);
+  FormatterToXML formatter(writer);
+  FormatterTreeWalker tree_walker(formatter);
+  tree_walker.traverse( node );
+  return target_stream.str();
+}
+
+
+XALAN_USING_XALAN(XalanNode)
+
+
+XalanNode*
+emu::utils::getSingleNode( XalanDocument* document, XalanNode* contextNode, const std::string xPath )
+throw( xcept::Exception )
+{
+  XALAN_USING_XALAN(XalanNode);
+  XalanNode* theNode;
+
+  XALAN_USING_XALAN(XSLException);
+
+  try
+  {
+    XALAN_USING_XERCES(XMLPlatformUtils);
+    XMLPlatformUtils::Initialize();
+    XALAN_USING_XALAN(XPathEvaluator);
+    XPathEvaluator::initialize();
+
+    // We'll use these to parse the XML file.
+    XALAN_USING_XALAN(XalanSourceTreeDOMSupport);
+    XalanSourceTreeDOMSupport theDOMSupport;
+    XALAN_USING_XALAN(XalanDocumentPrefixResolver);
+    XalanDocumentPrefixResolver thePrefixResolver(document);
+    XPathEvaluator theEvaluator;
+
+    XALAN_USING_XALAN(XalanDOMString);
+    theNode = theEvaluator.selectSingleNode(theDOMSupport, contextNode,
+                                            XalanDOMString(xPath.c_str()).c_str(), thePrefixResolver);
+    XMLPlatformUtils::Terminate();
+    XPathEvaluator::terminate();
+  }
+  catch (xcept::Exception& e)
+  {
+    std::stringstream ss;
+    ss << "XPath selection failed for '" << xPath << "': ";
+    XCEPT_RETHROW( xcept::Exception, ss.str(), e);
+  }
+  catch (XSLException& e)
+  {
+    XALAN_USING_XALAN(XalanDOMString)
+    std::stringstream ss;
+    ss << "XPath selection failed for '" << xPath << "': XSLException type: " << XalanDOMString(e.getType()) << ", message: " << e.getMessage();
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+  catch (std::exception& e)
+  {
+    std::stringstream ss;
+    ss << "XPath selection failed for '" << xPath << "': " << e.what();
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+  catch (...)
+  {
+    std::stringstream ss;
+    ss << "XPath selection failed for '" << xPath << "': Unknown exception.";
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+  return theNode;
+}
+
+
+XALAN_USING_XALAN(NodeRefList)
+
+
+void emu::utils::getNodes( NodeRefList& resultNodeList, XalanDocument* document, XalanNode* contextNode, const std::string xPath )
+throw( xcept::Exception )
+{
+  XALAN_USING_XALAN(XSLException);
+
+  try
+  {
+    XALAN_USING_XERCES(XMLPlatformUtils);
+    XMLPlatformUtils::Initialize();
+    XALAN_USING_XALAN(XPathEvaluator);
+    XPathEvaluator::initialize();
+
+    // We'll use these to parse the XML file.
+    XALAN_USING_XALAN(XalanSourceTreeDOMSupport);
+    XalanSourceTreeDOMSupport theDOMSupport;
+    XALAN_USING_XALAN(XalanDocumentPrefixResolver);
+    XalanDocumentPrefixResolver thePrefixResolver(document);
+    XPathEvaluator theEvaluator;
+
+    XALAN_USING_XALAN(XalanDOMString);
+    theEvaluator.selectNodeList(resultNodeList, theDOMSupport, contextNode,
+                                XalanDOMString(xPath.c_str()).c_str(), thePrefixResolver);
+    XPathEvaluator::terminate();
+    XMLPlatformUtils::Terminate();
+  }
+  catch (xcept::Exception& e)
+  {
+    std::stringstream ss;
+    ss << "XPath selection failed for '" << xPath << "': ";
+    XCEPT_RETHROW( xcept::Exception, ss.str(), e);
+  }
+  catch (XSLException& e)
+  {
+    XALAN_USING_XALAN(XalanDOMString)
+    std::stringstream ss;
+    ss << "XPath selection failed for '" << xPath << "': XSLException type: " << XalanDOMString(e.getType()) << ", message: " << e.getMessage();
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+  catch (std::exception& e)
+  {
+    std::stringstream ss;
+    ss << "XPath selection failed for '" << xPath << "': " << e.what();
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+  catch (...)
+  {
+    std::stringstream ss;
+    ss << "XPath selection failed for '" << xPath << "': Unknown exception.";
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+}
+
+
+std::string emu::utils::serializeSelectedNode( const std::string& XML, const std::string xPath )
+throw( xcept::Exception )
+{
+  std::string serializedNode;
+
+  XALAN_USING_XALAN(XercesDOMSupport)
+  XALAN_USING_XALAN(XercesParserLiaison)
+  XALAN_USING_XERCES(MemBufInputSource)
+  XALAN_USING_XERCES(XMLException)
+  XALAN_USING_XERCES(SAXParseException)
+  XALAN_USING_XALAN(XalanDOMException)
+  XALAN_USING_XALAN(XSLException)
+
+  try
+  {
+    XALAN_USING_XERCES(XMLPlatformUtils)
+    XALAN_USING_XALAN(XPathEvaluator)
+    XMLPlatformUtils::Initialize();
+
+    XercesDOMSupport theDOMSupport;
+    XercesParserLiaison theLiaison(theDOMSupport);
+    theLiaison.setDoNamespaces(true); // although it seems to be already set...
+    theLiaison.setBuildWrapperNodes(true);
+    theLiaison.setBuildMaps(true);
+    
+    const char* const id = "dummy";
+    MemBufInputSource theInputSource((const XMLByte*) XML.c_str(), (unsigned int) XML.size(), id);
+    XalanDocument* document = theLiaison.parseXMLStream(theInputSource);
+
+    serializedNode = serialize(getSingleNode(document, document, xPath));
+
+    XMLPlatformUtils::Terminate();
+  }
+  catch (SAXParseException& e)
+  {
+    std::stringstream ss;
+    ss << "Failed to serialize selected node: " << xoap::XMLCh2String(e.getMessage());
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+  catch (XMLException& e)
+  {
+    std::stringstream ss;
+    ss << "Failed to serialize selected node: " << xoap::XMLCh2String(e.getMessage());
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+  catch (DOMException& e)
+  {
+    std::stringstream ss;
+    ss << "Failed to serialize selected node: " << xoap::XMLCh2String(e.getMessage());
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+  catch (XalanDOMException& e)
+  {
+    std::stringstream ss;
+    ss << "Failed to serialize selected node: exception code " << e.getExceptionCode();
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+  catch (XSLException& e)
+  {
+    XALAN_USING_XALAN(XalanDOMString)
+    std::stringstream ss;
+    ss << "Failed to serialize selected node: XSLException type: " << XalanDOMString(e.getType()) << ", message: " << e.getMessage();
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+  catch (xcept::Exception& e)
+  {
+    XCEPT_RETHROW( xcept::Exception, "Failed to serialize selected node: ", e);
+  }
+  catch (std::exception& e)
+  {
+    std::stringstream ss;
+    ss << "Failed to serialize selected node: " << e.what();
+    XCEPT_RAISE( xcept::Exception, ss.str());
+  }
+  catch (...)
+  {
+    XCEPT_RAISE( xcept::Exception, "Failed to serialize selected node: Unknown exception.");
+  }
+  
+  return serializedNode;
+}
