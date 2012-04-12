1f1520fa4658c56dfd25ecbb1f960ce7ccc05da3
khotilov <khotilov>
2012-04-12 05:30:31 +0000

    get rid of the useless throw in declarations; switch to throwing emu::exception::Exception based exceptions; updates from Karoly; clean-up

--- base/include/emu/utils/System.h
+++ base/include/emu/utils/System.h
@@ -1,9 +1,6 @@
 #ifndef __emu_utils_System_h__
 #define __emu_utils_System_h__
 
-#include "log4cplus/logger.h"
-#include "xcept/Exception.h"
-
 #include <string>
 #include <vector>
 
@@ -12,27 +9,44 @@ namespace emu { namespace utils {
 
 /**
  * read text file to string
+ * @param fileName Name of file to read.
+ * @return Content of the file.
  */
-std::string readFile(const std::string fileName) throw (xcept::Exception);
+std::string readFile(const std::string &fileName);
 
 /**
- * write string to text file
+ * Create a file and write \c content into it.
+ * @param fileName Name of file to create.
+ * @param content Content of file to write to \c fileName
  */
-void writeFile(const std::string fileName, const std::string content) throw (xcept::Exception);
+void writeFile(const std::string &fileName, const std::string &content);
 
 /**
+ * Execute the argument in a shell.
+ * @param shellCommand
+ * @return A vector of lines of the reply.
+ */
+std::vector< std::string > execShellCommand(const std::string &shellCommand);
+
+/**
+ * Have the shell perform expansions (parameter, file name, etc.) on the argument.
+ * @param toExpand The string to expand.
+ * @return The (parameter, file name, etc.)-expanded string.
  *
  */
-std::vector< std::string > execShellCommand(const std::string shellCommand) throw (xcept::Exception);
+std::string performExpansions(const std::string &toExpand);
 
 /**
- * Gets UTC date and time.
- * @return UTC date and time in YYYYMMDD_hhmmss_UTC format
- */
-std::string getDateTime();
-
-/**
+ * Returns string containing date and time with the following default format:
+ *   YYYY-MM-DD HH:MM:SS
  *
+ * @param for_file_name  if true, the format would be
+ *   YYYY-MM-DD_HH-MM-SS
+ */
+std::string getDateTime(bool for_file_name = false);
+
+/**
+ * Holder for SCSI device information
  */
 struct SCSI_t
 {
@@ -46,9 +60,15 @@ struct SCSI_t
 };
 
 /**
- *
+ * @return SCSI_t type structure for a first SCSI device in /proc/scsi/scsi that matches given \c vendor and \c model
  */
-SCSI_t getSCSI(const std::string vendor, const std::string model);
+SCSI_t getSCSI(const std::string &vendor, const std::string &model);
+
+
+/**
+ * @return device ID string (like /dev/sg0 ) for a device specified by \c scsi
+ */
+std::string findSCSIDevice( SCSI_t scsi );
 
 }}
 
--- base/src/common/utils/System.cc
+++ base/src/common/utils/System.cc
@@ -1,8 +1,9 @@
-// $Id: System.cc,v 1.1 2012/04/11 21:34:47 khotilov Exp $
+// $Id: System.cc,v 1.2 2012/04/12 05:30:31 khotilov Exp $
 
 #include "emu/utils/System.h"
 #include "emu/utils/IO.h"
 #include "emu/utils/String.h"
+#include "emu/exception/Exception.h"
 
 #include "toolbox/regex.h"
 
@@ -16,8 +17,7 @@
 
 //using namespace std;
 
-std::string emu::utils::readFile( const std::string fileName )
-throw ( xcept::Exception )
+std::string emu::utils::readFile( const std::string &fileName )
 {
   std::stringstream ss;
   try
@@ -33,15 +33,14 @@ throw ( xcept::Exception )
   {
     std::stringstream ess;
     ess << "Failed to read file \"" << fileName << "\": " << e.what();
-    XCEPT_RAISE( xcept::Exception, ess.str());
+    XCEPT_RAISE( emu::exception::FileException, ess.str());
   }
   //cout << "emu::utils::readFile" << endl << ss.str() << endl;
   return ss.str();
 }
 
 
-void emu::utils::writeFile( const std::string fileName, const std::string content )
-throw ( xcept::Exception )
+void emu::utils::writeFile( const std::string &fileName, const std::string &content )
 {
   try
   {
@@ -52,13 +51,12 @@ throw ( xcept::Exception )
   {
     std::stringstream ess;
     ess << "Failed to write file \"" << fileName << "\": " << e.what();
-    XCEPT_RAISE( xcept::Exception, ess.str());
+    XCEPT_RAISE( emu::exception::FileException, ess.str());
   }
 }
 
 
-std::vector< std::string >
-emu::utils::execShellCommand(const std::string shellCommand) throw (xcept::Exception)
+std::vector< std::string > emu::utils::execShellCommand(const std::string &shellCommand)
 {
   std::vector< std::string > replyLines;
   try
@@ -74,35 +72,60 @@ emu::utils::execShellCommand(const std::string shellCommand) throw (xcept::Excep
   {
     std::stringstream ess;
     ess << "Failed to execute shell command \"" << shellCommand << "\": " << e.what();
-    XCEPT_RAISE( xcept::Exception, ess.str());
+    XCEPT_RAISE( emu::exception::FileException, ess.str());
   }
   return replyLines;
 }
 
 
-std::string emu::utils::getDateTime()
+std::string emu::utils::performExpansions(const std::string &toExpand)
+{
+  std::string expanded;
+  try
+  {
+    redi::ipstream command( ( std::string("echo ") + toExpand ).c_str() );
+    std::string reply;
+    while ( std::getline( command, reply ) )
+    {
+      expanded += reply;
+    }
+  }
+  catch( std::exception& e )
+  {
+    std::stringstream ess;
+    ess << "Expansion failed. Failed to execute shell command \"echo " << toExpand << "\": " << e.what();
+    XCEPT_RAISE( emu::exception::FileException, ess.str() );
+  }
+  return expanded;
+}
+
+
+std::string emu::utils::getDateTime(bool for_file_name)
 {
   using namespace std;
 
   time_t t;
-  struct tm *tm;
+  struct tm tm;
   time(&t);
-  tm = localtime(&t);
+  localtime_r(&t, &tm);
+
+  std::string gap0 = "-";
+  std::string gap1 = (for_file_name) ? "_" : " ";
+  std::string gap2 = (for_file_name) ? gap0 : ":";
 
   std::stringstream ss;
-  ss << setfill('0') << setw(4) << tm->tm_year+1900 << "-"
-     << setfill('0') << setw(2) << tm->tm_mon+1     << "-"
-     << setfill('0') << setw(2) << tm->tm_mday      << "T"
-     << setfill('0') << setw(2) << tm->tm_hour      << ":"
-     << setfill('0') << setw(2) << tm->tm_min       << ":"
-     << setfill('0') << setw(2) << tm->tm_sec;
+  ss << setfill('0') << setw(4) << tm.tm_year+1900 << gap0
+     << setfill('0') << setw(2) << tm.tm_mon+1     << gap0
+     << setfill('0') << setw(2) << tm.tm_mday      << gap1
+     << setfill('0') << setw(2) << tm.tm_hour      << gap2
+     << setfill('0') << setw(2) << tm.tm_min       << gap2
+     << setfill('0') << setw(2) << tm.tm_sec;
 
   return ss.str();
 }
 
 
-emu::utils::SCSI_t
-emu::utils::getSCSI( const std::string vendor, const std::string model )
+emu::utils::SCSI_t emu::utils::getSCSI( const std::string &vendor, const std::string &model )
 {
   using namespace std;
 
@@ -143,22 +166,70 @@ emu::utils::getSCSI( const std::string vendor, const std::string model )
         if (scsi.vendor.compare(vendor) == 0 && scsi.model.find(model) == 0) return scsi;
       }
     }
-    XCEPT_RAISE( xcept::Exception, string( "No SCSI device of vendor \"" ) + vendor + "\" and model \"" + model + "\" found.");
+    XCEPT_RAISE( emu::exception::DevTypeException, string( "No SCSI device of vendor \"" ) + vendor + "\" and model \"" + model + "\" found.");
   }
   catch (xcept::Exception &e)
   {
-    XCEPT_RETHROW( xcept::Exception, "Failed to get SCSI parameters.", e);
+    XCEPT_RETHROW( emu::exception::Exception, "Failed to get SCSI parameters.", e);
   }
   catch (std::exception &e)
   {
     stringstream ss;
     ss << "Failed to get SCSI parameters. " << e.what();
-    XCEPT_RAISE( xcept::Exception, ss.str() );
+    XCEPT_RAISE( emu::exception::Exception, ss.str() );
   }
   catch (...)
   {
-    XCEPT_RAISE( xcept::Exception, "Unknown exception." );
+    XCEPT_RAISE( emu::exception::UndefinedException, "Unknown exception." );
   }
 
   return scsi;
+}
+
+
+std::string emu::utils::findSCSIDevice( emu::utils::SCSI_t scsi )
+{
+  const char *fullPathTo_devices     = "/proc/scsi/sg/devices";
+  const char *fullPathTo_device_strs = "/proc/scsi/sg/device_strs";
+
+  int host, chan, id, lun, type, opens, qdepth, busy, online; // See /proc/scsi/sg/device_hdr
+  std::string vendor, model;
+  const int nGenericDevices = 256; // There are 256 possible SCSI generic (sg) devices: /dev/sg{0..255}
+
+  std::fstream devices    ( fullPathTo_devices    , std::fstream::in );
+  std::fstream device_strs( fullPathTo_device_strs, std::fstream::in );
+  int iLine = 0; // The line number corresponds to the SCSI generic device number in /dev/sg<N>
+
+  while ( devices.good() && device_strs.good() && iLine < nGenericDevices )
+  {
+    const int lineLength = 256;
+    char line_devices[lineLength];
+    char line_device_strs[lineLength];
+    devices    .getline( line_devices    , lineLength );
+    device_strs.getline( line_device_strs, lineLength );
+    if ( ! devices.fail() && ! device_strs.fail() )
+    {
+      std::stringstream ss_line_devices(line_devices);
+      ss_line_devices >> host >> chan >> id >> lun >> type >> opens >> qdepth >> busy >> online;
+
+      if ( scsi.host    == host &&
+           scsi.channel == chan &&
+           scsi.id      == id   &&
+           scsi.lun     == lun     )
+      {
+        std::stringstream ss_line_device_strs( line_device_strs );
+        ss_line_device_strs >> vendor >> model;
+        // Require strict match for the vendor name. As for the model name, it's enough if it starts with the given string.
+        if ( scsi.vendor.compare( vendor ) == 0 && scsi.model.find( model ) == 0 )
+        {
+          std::stringstream scsiDevice;
+          scsiDevice << "/dev/sg" << iLine;
+          return scsiDevice.str();
+        }
+      }
+    }
+    ++iLine;
+  }
+
+  return std::string("");
 }
