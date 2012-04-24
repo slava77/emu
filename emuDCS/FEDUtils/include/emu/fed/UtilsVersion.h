8304c2cd858333fd63d8f0b09a65e48ebc48790c
liu <liu@4525493e-7705-40b1-a816-d608a930855b>
2012-04-24 15:27:47 +0000

    version 11.2
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@6822 4525493e-7705-40b1-a816-d608a930855b

--- emuDCS/FEDUtils/include/emu/fed/UtilsVersion.h
+++ emuDCS/FEDUtils/include/emu/fed/UtilsVersion.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
-* $Id: UtilsVersion.h,v 1.51 2012/03/08 14:51:40 liu Exp $
+* $Id: UtilsVersion.h,v 1.52 2012/04/24 15:27:46 liu Exp $
 \*****************************************************************************/
 #ifndef __EMU_FED_UTILSVERSION_H__
 #define __EMU_FED_UTILSVERSION_H__
@@ -8,7 +8,7 @@
 
 namespace EmuFEDUtils {
 	const std::string package     = "EmuFEDUtils";
-	const std::string versions    = "11.01.00";
+	const std::string versions    = "11.02.00";
 	const std::string description = "Emu (CSC) FED Utility Libraries";
 
 	const std::string summary     = "Utility libraries used by the Front End Device (FED) crate XDAQ applications (FEDApps) for the CMS Emu (CSC) subdetector";
