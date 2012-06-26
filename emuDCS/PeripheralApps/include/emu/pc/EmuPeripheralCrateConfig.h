dbcde6a52218da19209174f39480e39b48f9cf4f
liu <liu@4525493e-7705-40b1-a816-d608a930855b>
2012-06-26 14:29:28 +0000

    add firmware downloading for new TMB and ALCT
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@6869 4525493e-7705-40b1-a816-d608a930855b

--- emuDCS/PeripheralApps/include/emu/pc/EmuPeripheralCrateConfig.h
+++ emuDCS/PeripheralApps/include/emu/pc/EmuPeripheralCrateConfig.h
@@ -439,6 +439,8 @@ private:
   void TMBReadFirmware(xgi::Input * in, xgi::Output * out ) throw (xgi::exception::Exception);
   void ALCTReadFirmware(xgi::Input * in, xgi::Output * out ) throw (xgi::exception::Exception);
   void LoadALCTSlowFirmware(xgi::Input * in, xgi::Output * out ) throw (xgi::exception::Exception);
+  void LoadVirtex6TMBFirmware(xgi::Input * in, xgi::Output * out ) throw (xgi::exception::Exception);  
+  void LoadSpartan6ALCTFirmware(xgi::Input * in, xgi::Output * out ) throw (xgi::exception::Exception);
   //
   // VCC utils
   void VCCHeader(xgi::Input * in, xgi::Output * out, std::string title, std::string heading ) throw (xgi::exception::Exception); 
