da05f7a36229135e76a08c04cf49d3410a44e0f3
cvuosalo <cvuosalo@4525493e-7705-40b1-a816-d608a930855b>
2012-08-14 11:38:10 +0000

    Added new expert system facts for DDU errors and DDU stuck in Warning
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@6924 4525493e-7705-40b1-a816-d608a930855b

--- emuDCS/FEDApps/include/emu/fed/Facts.h
+++ emuDCS/FEDApps/include/emu/fed/Facts.h
@@ -1,12 +1,15 @@
 /*****************************************************************************\
-* $Id: Facts.h,v 1.4 2010/08/13 03:00:07 paste Exp $
+* $Id: Facts.h,v 1.5 2012/08/14 11:38:10 cvuosalo Exp $
 \*****************************************************************************/
 #ifndef __EMU_FED_FACTS__
 #define __EMU_FED_FACTS__
 
 #include "emu/fed/Fact.h"
 #include "emu/fed/FEDFSMFact.h"
-#include "emu/fed/DDUFMMIRQFact.h"
+#include "emu/fed/DDUFMMIRQFact.h" // Obsolete?
 #include "emu/fed/DDUFMMResetFact.h"
+
+#include "emu/fed/DduFmmErrorFact.h" 
+#include "emu/fed/DduStuckInWarningFact.h" 
 
 #endif
