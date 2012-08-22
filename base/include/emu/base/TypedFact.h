d290d8d7643e110d1e1bf5d07a86e5598fe2cb29
barvic <barvic@4525493e-7705-40b1-a816-d608a930855b>
2012-08-22 18:54:26 +0000

    Fix order of parameters in fact XML. Support array parameters.
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@6938 4525493e-7705-40b1-a816-d608a930855b

--- base/include/emu/base/TypedFact.h
+++ base/include/emu/base/TypedFact.h
@@ -3,13 +3,18 @@
 
 #include "emu/base/Fact.h"
 
+#include "emu/utils/String.h"
+
 
 namespace emu { namespace base {
 
   template <class T>
   class TypedFact : public Fact {
   public:
-    TypedFact<T>() : Fact( T::getTypeName() ){}
+    TypedFact<T>() : Fact( T::getTypeName() ){
+      parameters_.resize( T::nParameters );
+      for ( size_t i = 0; i < T::nParameters; ++i ) parameters_[i].first = T::getParameterName( (typename T::ParameterName_t) i );
+    }
 
     TypedFact<T>& setRun        ( const string& run                      ){ run_         = run;              return *this; }
     TypedFact<T>& setTime       ( const string& time                     ){ time_        = time;             return *this; }
@@ -19,13 +24,47 @@ namespace emu { namespace base {
     TypedFact<T>& setUnit       ( const emu::base::Fact::Unit_t        u ){ unit_        = units_[u];        return *this; }
     TypedFact<T>& setSeverity   ( const emu::base::Fact::Severity_t    s ){ severity_    = severities_[s];   return *this; }
 
+    /// Set scalar parameter
+    ///
+    /// @param name Name of parameter.
+    /// @param value Value of parameter.
+    ///
+    /// @return Reference to this object.
+    ///
     template <typename V>
     TypedFact<T>& setParameter( const typename T::ParameterName_t name, const V& value ){
-      stringstream ss;
-      ss << value;
-      parameters_[ T::getParameterName(name) ] = ss.str();
+      // Scalar parameter is also stored as vector:
+      vector<string> vs( 1, emu::utils::stringFrom<V>( value ) );
+      parameters_[ name ].second.swap( vs );
       return *this;
     }
+
+    /// Set array parameter
+    ///
+    /// @param name Name of parameter.
+    /// @param values STL vector of parameter values.
+    ///
+    /// @return  Reference to this object.
+    ///
+    template <typename V>
+    TypedFact<T>& setParameter( const typename T::ParameterName_t name, const vector<V>& values ){
+      vector<string> vs( stringify( values ) );
+      parameters_[ name ].second.swap( vs );
+      return *this;
+    }
+
+  protected:
+
+    template <typename V>
+    vector<string> stringify( const vector<V>& values ){
+      vector<string> strings;
+      typename vector<V>::const_iterator iv;
+      for ( iv = values.begin(); iv != values.end(); ++iv ){
+	strings.push_back( emu::utils::stringFrom<V>( *iv ) );
+      }
+      return strings;
+    }
+
   };
 
 }}
