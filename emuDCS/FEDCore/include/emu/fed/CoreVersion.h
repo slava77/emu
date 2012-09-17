4f461c3541a07c304040655fb55bc5519c39a5f7
liu <liu>
2012-09-17 02:37:21 +0000

    version 11.4

--- emuDCS/FEDCore/include/emu/fed/CoreVersion.h
+++ emuDCS/FEDCore/include/emu/fed/CoreVersion.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
-* $Id: CoreVersion.h,v 1.55 2012/06/26 14:37:54 liu Exp $
+* $Id: CoreVersion.h,v 1.56 2012/09/17 02:37:20 liu Exp $
 \*****************************************************************************/
 
 #ifndef __EMU_FED_COREVERSION_H__
@@ -9,7 +9,7 @@
 
 namespace EmuFEDCore {
 	const std::string package     = "EmuFEDCore";
-	const std::string versions    = "11.03.00";
+	const std::string versions    = "11.04.00";
 	const std::string description = "Emu (CSC) FED Core Libraries";
 
 	const std::string summary     = "Core libraries for controlling Front End Device (FED) crates for the CMS Emu (CSC) subdetector";
--- emuDCS/FEDUtils/include/emu/fed/UtilsVersion.h
+++ emuDCS/FEDUtils/include/emu/fed/UtilsVersion.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
-* $Id: UtilsVersion.h,v 1.53 2012/06/26 14:37:55 liu Exp $
+* $Id: UtilsVersion.h,v 1.54 2012/09/17 02:37:20 liu Exp $
 \*****************************************************************************/
 #ifndef __EMU_FED_UTILSVERSION_H__
 #define __EMU_FED_UTILSVERSION_H__
@@ -8,7 +8,7 @@
 
 namespace EmuFEDUtils {
 	const std::string package     = "EmuFEDUtils";
-	const std::string versions    = "11.03.00";
+	const std::string versions    = "11.04.00";
 	const std::string description = "Emu (CSC) FED Utility Libraries";
 
 	const std::string summary     = "Utility libraries used by the Front End Device (FED) crate XDAQ applications (FEDApps) for the CMS Emu (CSC) subdetector";
