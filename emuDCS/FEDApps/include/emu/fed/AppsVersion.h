8a142076c6ecccd3c871f06810df614835e0493a
banicz <banicz@4525493e-7705-40b1-a816-d608a930855b>
2012-11-25 23:12:31 +0000

    Version 11.04.03
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@7046 4525493e-7705-40b1-a816-d608a930855b

--- emuDCS/FEDApps/include/emu/fed/AppsVersion.h
+++ emuDCS/FEDApps/include/emu/fed/AppsVersion.h
@@ -1,5 +1,5 @@
 /*****************************************************************************\
-* $Id: AppsVersion.h,v 1.84 2012/10/30 15:05:53 banicz Exp $
+* $Id: AppsVersion.h,v 1.85 2012/11/25 23:12:31 banicz Exp $
 \*****************************************************************************/
 
 #ifndef __EMU_FED_APPSVERSION_H__
@@ -9,7 +9,7 @@
 
 namespace EmuFEDApps {
 	const std::string package     = "EmuFEDApps";
-	const std::string versions    = "11.04.02";
+	const std::string versions    = "11.04.03";
 	const std::string description = "Emu (CSC) FED XDAQ Applications";
 
 	const std::string summary     = "Applications to control and monitor the Front End Device (FED) crates for the CMS Emu (CSC) subdetector";
