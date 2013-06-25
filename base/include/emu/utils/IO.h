cbffb350f0211fd31e299a6f43ff6d634fa9d178
banicz <banicz@4525493e-7705-40b1-a816-d608a930855b>
2013-06-25 17:52:34 +0000

    Made available in more namespaces.
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@7452 4525493e-7705-40b1-a816-d608a930855b

--- base/include/emu/utils/IO.h
+++ base/include/emu/utils/IO.h
@@ -7,110 +7,20 @@
 #include <valarray>
 #include <iostream>
 
-namespace emu { namespace utils {
+namespace emu { 
 
-/**
- * quick vector dump
- */
-template< typename T >
-std::ostream& operator<<(std::ostream& os, const std::vector< T >& t)
-{
-  os << "[";
-  typename std::vector< T >::const_iterator it;
-  for (it = t.begin(); it != t.end(); ++it)
-    os << *it << (it + 1 != t.end() ? "," : "");
-  os << "]";
-  return os;
-}
-
-template< typename T >
-std::ostream& operator<<(std::ostream& os, std::vector< T >& t)
-{
-  os << "[";
-  typename std::vector< T >::iterator it;
-  for (it = t.begin(); it != t.end(); ++it)
-    os << *it << (it + 1 != t.end() ? "," : "");
-  os << "]";
-  return os;
-}
-
-/**
- * quick set dump
- */
-template < typename T >
-std::ostream& operator<<(std::ostream& os, const std::set<T>& t)
-{
-  os << "{";
-  if ( t.size() )
-    {
-      typename std::set<T>::iterator it, iti;
-      for ( it = iti = t.begin(), ++iti; it != t.end(); ++it, ++iti )
-	os << *it << (iti != t.end() ? "," : "");
-    }
-  os << "}";
-  return os;
-}
-
-/**
- * quick pair dump
- */
-template< typename T1, typename T2 >
-std::ostream& operator<<(std::ostream& os, const std::pair< T1, T2 >& t)
-{
-  os << "(" << t.first << "|" << t.second << ")";
-  return os;
-}
-
-/**
- * quick map dump
- */
-template< typename T1, typename T2 >
-std::ostream& operator<<(std::ostream& os, const std::map< T1, T2 >& t)
-{
-  os << "(";
-  if ( t.size() )
-    {
-      typename std::map< T1, T2 >::const_iterator it, iti;
-      for (it = iti = t.begin(), ++iti; it != t.end(); ++it, ++iti)
-	os << it->first << "->" << it->second << (iti != t.end() ? "," : "");
-    }
-  os << ")";
-  return os;
-}
-
-/**
- * quick multimap dump
- */
-template< typename T1, typename T2 >
-std::ostream& operator<<(std::ostream& os, const std::multimap< T1, T2 >& t)
-{
-  os << "(";
-  if ( t.size() )
-    {
-      typename std::multimap< T1, T2 >::const_iterator it, iti;
-      for (it = iti = t.begin(), ++iti; it != t.end(); ++it, ++iti)
-	os << it->first << "->" << it->second << (iti != t.end() ? "," : "");
-    }
-  os << ")";
-  return os;
-}
-
-/**
- * quick valarray dump
- */
-template < typename T >
-std::ostream& operator<<(std::ostream& os, const std::valarray<T>& t)
-{
-  os << "[";
-  size_t i=0;
-  while( i+1 < t.size() ){
-    os << t[i] << ", ";
-    ++i;
+  namespace utils {
+#include "emu/utils/IO.i"
   }
-  os << t[i] << "]";
-  return os;
-}
 
-}}
+  namespace pc {
+#include "emu/utils/IO.i"
+  }
+
+  namespace step {
+#include "emu/utils/IO.i"
+  }
+
+}
 
 #endif
