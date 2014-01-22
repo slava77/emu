c07446f469ee8789608f8fa67411669ed9e61ffc
banicz <banicz@4525493e-7705-40b1-a816-d608a930855b>
2014-01-22 15:12:53 +0000

    Driver for the Intel dual port PRO/1000 PF NIC with updated hooks for DDU, DMB and VME.
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@7646 4525493e-7705-40b1-a816-d608a930855b

---
+++ drivers/e1000_emu/eth_hook_4_daq/eth_hook_4_daq.h
@@ -0,0 +1,11 @@
+//#define PAGE_SIZE 4096
+#define BIGPHYS_PAGES_4 5000
+#define RING_PAGES_4 1000
+#define RING_ENTRY_LENGTH 8
+#define MAXPACKET_4 9100
+//#define MAXEVENT_4 30100
+#define MAXEVENT_4 180000
+#define TAILMEM 100
+#define TAILPOS  80
+#define SKB_EXTRA 8 // skb->len is not the actual lenght. skb->len+SKB_EXTRA is.
+#define SKB_OFFSET -14 // Data doesn't start at skb->data, but at skb->data+SKB_OFFSET.
