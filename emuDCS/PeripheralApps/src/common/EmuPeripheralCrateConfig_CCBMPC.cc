1ae781dfcb1f2fd1bcc4faba4f716f15a2790175
liu <liu@4525493e-7705-40b1-a816-d608a930855b>
2014-05-13 15:20:02 +0000

    update CCB Status page to include some TTC counters
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@7779 4525493e-7705-40b1-a816-d608a930855b

--- emuDCS/PeripheralApps/src/common/EmuPeripheralCrateConfig_CCBMPC.cc
+++ emuDCS/PeripheralApps/src/common/EmuPeripheralCrateConfig_CCBMPC.cc
@@ -120,6 +120,15 @@ void EmuPeripheralCrateConfig::CCBStatus(xgi::Input * in, xgi::Output * out )
   *out << cgicc::br() << "CSRB1 =  " << std::hex << thisCCB->ReadRegister(0x20) << std::endl;
   *out << cgicc::br() << "CSRB18 = " << std::hex << thisCCB->ReadRegister(0x42) << std::endl;
   //
+  unsigned lastcmd=(thisCCB->ReadRegister(0x3C) & 0xff)>>2;
+  unsigned bcounter=thisCCB->ReadRegister(0x44) + thisCCB->ReadRegister(0x46)* 65536; 
+  unsigned dcounter=thisCCB->ReadRegister(0x48); 
+  unsigned lcounter=thisCCB->ReadRegister(0x90) + thisCCB->ReadRegister(0x92)* 65536; 
+  *out << cgicc::br() << cgicc::br() << "L1ACC counter =  " << lcounter << std::endl;
+  *out << cgicc::br() << "BRCST counter =  " << bcounter << std::endl;
+  *out << cgicc::br() << "DOUT  counter =  " << dcounter << std::endl;
+  *out << cgicc::br() << "Last TTC command (hex) =  " <<  std::hex << lastcmd << std::dec << std::endl;
+  
   *out << cgicc::fieldset();
   //
 }
