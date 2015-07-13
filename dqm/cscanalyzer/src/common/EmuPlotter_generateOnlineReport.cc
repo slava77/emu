6e1f21dcf28ffa3a93debd59c067adb2b5f388d6
barvic <barvic@4525493e-7705-40b1-a816-d608a930855b>
2015-07-13 16:11:07 +0000

    Adjustments for dead comparators channels test.
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@8189 4525493e-7705-40b1-a816-d608a930855b

--- dqm/cscanalyzer/src/common/EmuPlotter_generateOnlineReport.cc
+++ dqm/cscanalyzer/src/common/EmuPlotter_generateOnlineReport.cc
@@ -1138,10 +1138,10 @@ int EmuPlotter::generateOnlineReport(std::string runname)
                                           for (int ch=1; ch <=32; ch++)
                                             {
                                               double ch_val = h->GetBinContent(ch+icfeb*32);
-                                              if (( ch<2 && icfeb==0 ) || ( ch>31  && (icfeb==(nCFEBs-1)) )
-                                                  || ( ME11 && ch<2 && icfeb==4 ) || ( ME11 && ch>31 && icfeb==3 )
-                                                  || ( ch<3 && icfeb>0 && (badCFEBs[icfeb-1]==1))
-                                                  || ( ch>31 && icfeb<(nCFEBs-1) && (badCFEBs[icfeb+1]==1)) ) continue;
+                                              if (( ch<=3 && icfeb==0 ) || ( ch>=31  && (icfeb==(nCFEBs-1)) )
+                                                  || ( ME11 && ch<=3 && icfeb==4 ) || ( ME11 && ch>=31 && icfeb==3 )
+                                                  || ( ch<=3 && icfeb>0 && (badCFEBs[icfeb-1]==1))
+                                                  || ( ch>=31 && icfeb<(nCFEBs-1) && (badCFEBs[icfeb+1]==1)) ) continue;
                                               if ( (nentries >= (40*32*nActiveCFEBs)) && (ch_val <= 0.05*avg_comp_ch_occupancy)
                                                    && !isDeadLowComps && !isNoisyComps && !isNoisyCompsChan )
                                                 {
@@ -1217,10 +1217,10 @@ int EmuPlotter::generateOnlineReport(std::string runname)
                                       for (int ch=1; ch <=32; ch++)
                                         {
                                           double ch_val = h->GetBinContent(ch+icfeb*32);
-                                          if ( ( ch<2 && icfeb==0 ) || ( ch>31  && icfeb==(nCFEBs-1) )
-                                               || ( ME11 && ch<2 && icfeb==4 ) || ( ME11 && ch>31 && icfeb==3 )
-                                               || ( ch<3 && icfeb>0 && (badCFEBs[icfeb-1]==1))
-                                               || ( ch>31 && icfeb<(nCFEBs-1) && (badCFEBs[icfeb+1]==1)) ) continue;
+                                          if ( ( ch<=3 && icfeb==0 ) || ( ch>=31  && icfeb==(nCFEBs-1) )
+                                               || ( ME11 && ch<=3 && icfeb==4 ) || ( ME11 && ch>=31 && icfeb==3 )
+                                               || ( ch<=3 && icfeb>0 && (badCFEBs[icfeb-1]==1))
+                                               || ( ch>=31 && icfeb<(nCFEBs-1) && (badCFEBs[icfeb+1]==1)) ) continue;
                                           if ( (nentries >= (40*32*nActiveCFEBs)) && (ch_val <= 0.05*avg_comp_ch_occupancy)
                                                && !isDeadLowComps && !isNoisyComps && !isNoisyCompsChan)
                                             {
