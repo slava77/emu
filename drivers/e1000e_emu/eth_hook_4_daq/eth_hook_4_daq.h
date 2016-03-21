adac0c9904b6102a07995ae5ed6ef0e173536e1d
banicz <banicz@4525493e-7705-40b1-a816-d608a930855b>
2016-03-21 14:43:48 +0000

    Removed floating point operation from rate calculation. (Causes crash in SLC6.)
    git-svn-id: svn+ssh://svn.cern.ch/reps/emu/trunk/TriDAS/emu@8315 4525493e-7705-40b1-a816-d608a930855b

--- drivers/e1000e_emu/eth_hook_4_daq/eth_hook_4_daq.h
+++ drivers/e1000e_emu/eth_hook_4_daq/eth_hook_4_daq.h
@@ -9,12 +9,8 @@
 #define TAILPOS  80
 
 #ifdef _DDU_SPY_
-// #define SKB_EXTRA   -2 // skb->len is not the actual lenght. skb->len+SKB_EXTRA is.
-// #define SKB_OFFSET -14 // Data doesn't start at skb->data, but at skb->data+SKB_OFFSET.
-// #define SKB_EXTRA   12 // skb->len is not the actual lenght. skb->len+SKB_EXTRA is.
-// #define SKB_OFFSET -14 // Data doesn't start at skb->data, but at skb->data+SKB_OFFSET.
-#define SKB_EXTRA -2 // skb->len is not the actual lenght. skb->len+SKB_EXTRA is.
-#define SKB_OFFSET 0 // Data doesn't start at skb->data, but at skb->data+SKB_OFFSET.
+#define SKB_EXTRA 12 // skb->len is not the actual lenght. skb->len+SKB_EXTRA is.
+#define SKB_OFFSET -14 // Data doesn't start at skb->data, but at skb->data+SKB_OFFSET.
 #endif
 
 #ifdef _DMB_SPY_
